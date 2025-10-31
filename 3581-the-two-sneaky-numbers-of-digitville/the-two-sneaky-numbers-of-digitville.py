3db78397fb317e4af22b3daafd8b4575cee5763d
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2025-10-31 07:14:16 +0530

    Time: 4 ms (51.56%) | Memory: 12.6 MB (12.28%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.py
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.py
@@ -4,11 +4,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        freq_arr = [0] * 101
+        freq = {}
         ans = []
         for i in range(len(nums)):
-            freq_arr[nums[i]] += 1
-            if freq_arr[nums[i]] == 2:
+            freq[nums[i]] = freq.get(nums[i], 0) + 1
+            if freq[nums[i]] == 2:
                 ans.append(nums[i])
             if len(ans) == 2:
                 break
