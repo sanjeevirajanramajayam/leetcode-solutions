69ac554d0ea5a83dc3cb51ca4d9d63eb2600c966
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2025-12-02 09:07:31 +0530

    Time: 111 ms (65.00%) | Memory: 57.1 MB (20.00%) - LeetSync

---
+++ 3886-count-number-of-trapezoids-i/count-number-of-trapezoids-i.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def countTrapezoids(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: int
+        """
+        hashmap = {}
+        count = 0
+        pairCount = 0
+        traps = 0
+        for i in range(len(points)):
+            hashmap[points[i][1]] = hashmap.get(points[i][1], 0) + 1
+        for i in list(hashmap.values()):
+            if i >= 2:
+                w = i * (i - 1) // 2
+                traps = (traps + pairCount * w) % (10**9 + 7)
+                pairCount += w
+        return traps
