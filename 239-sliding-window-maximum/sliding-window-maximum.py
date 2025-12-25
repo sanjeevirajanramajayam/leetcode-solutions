4a79501414f34de9ee837508069ee8caa6d98ea7
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2025-12-25 10:40:26 +0530

    Time: 287 ms (38.40%) | Memory: 30.1 MB (11.12%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -5,14 +5,14 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        res = []
+        ans = [0] * (len(nums) - (k - 1))
         dq = deque()
         for i in range(len(nums)):
+            while dq and nums[dq[-1]] <= nums[i]:
+                dq.pop()
             while dq and dq[0] <= i - k:
                 dq.popleft()
-            while dq and nums[dq[-1]] < nums[i]:
-                dq.pop()
             dq.append(i)
-            if i >= k - 1:
-                res.append(nums[dq[0]])
-        return list(res)
+            if i >= (k - 1):
+                ans[i - (k - 1)] = nums[dq[0]]      
+        return ans      
