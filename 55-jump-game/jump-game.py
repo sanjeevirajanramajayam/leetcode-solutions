d66c477fa45dbe15a7a96fca59fa67ca3b26f29f
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-01-11 14:30:09 +0530

    Time: 58 ms (10.61%) | Memory: 13.6 MB (7.47%) - LeetSync

---
+++ 55-jump-game/jump-game.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        maxIndex = 0
+        for i in range(len(nums)):
+            if i > maxIndex:
+                return False
+            maxIndex = max(maxIndex, i + nums[i])
+            if maxIndex == (len(nums) - 1):
+                return True
+        return True
+        
+        
