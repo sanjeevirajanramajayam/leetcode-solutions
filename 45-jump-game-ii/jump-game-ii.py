d81c8ec60067e468e3f9d75bb796ed62cb4a7a82
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-01-11 14:45:12 +0530

    Time: 59 ms (16.83%) | Memory: 13 MB (94.99%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def jump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        l = 0
+        r = 0
+        jumps = 0
+        while r < (len(nums) - 1):
+            furthest = -float('inf')
+            for i in range(l, r + 1):
+                furthest = max(furthest, i + nums[i])
+            l = r + 1
+            r = furthest
+            print(l, r)
+            jumps += 1
+        return jumps
