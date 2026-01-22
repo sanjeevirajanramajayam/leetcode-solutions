c9dd2e29d4a8407eeeb2401f6f28543d32af6321
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-01-22 21:45:04 +0530

    Time: 219 ms (47.37%) | Memory: 13.8 MB (22.37%) - LeetSync

--- 621-task-scheduler/task-scheduler.py
+++ 621-task-scheduler/task-scheduler.py
@@ -5,21 +5,21 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        time = 0
-        queue = deque()
         counter = {}
-        for i in range(len(tasks)):
-            counter[tasks[i]] = counter.get(tasks[i], 0) + 1
-        maxHeap = [-x for x in (counter.values())]
+        for task in tasks:
+            counter[task] = counter.get(task, 0) + 1
+        maxHeap = [-x for x in counter.values()]
+        queue = deque()
         heapq.heapify(maxHeap)
+        time = 0
         while maxHeap or queue:
             time += 1
             if maxHeap:
-                cnt = heapq.heappop(maxHeap) + 1
-                if cnt != 0:
-                    queue.append([cnt, time + n])
-            if queue and queue[0][1] == time:
-                cnt, _ = queue.popleft()
-                heapq.heappush(maxHeap, cnt)
+                task = -heapq.heappop(maxHeap)
+                if task - 1 > 0:   
+                    queue.append((task - 1, time + n))
 
+            if queue and queue[0][1] == time:
+                task, _ = queue.popleft()
+                heapq.heappush(maxHeap, -task)
         return time
