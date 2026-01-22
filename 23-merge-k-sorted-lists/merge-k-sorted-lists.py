49b2bfcf9bb139fca985f44bacc72803cfe1fbf0
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-01-22 21:02:18 +0530

    Time: 23 ms (40.48%) | Memory: 18.1 MB (89.87%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -9,37 +9,45 @@ class Solution(object):
         :type lists: List[Optional[ListNode]]
         :rtype: Optional[ListNode]
         """
-
-        if not lists:
-            return None
-
-        def combineLinkedLists(l1, l2):
-            dummy = ListNode()
-            curr = dummy
+        def mergeList(l1, l2):
+            curr = ListNode()
+            dummy = curr
             while l1 and l2:
-                if l1.val < l2.val:
-                    curr.next = l1
-                    curr = curr.next
-                    l1 = l1.next
-                else:
-                    curr.next = l2
-                    curr = curr.next
+                if l1.val > l2.val:
+                    dummy.next = l2
+                    dummy = dummy.next
                     l2 = l2.next
+                else:
+                    dummy.next = l1
+                    dummy = dummy.next
+                    l1 = l1.next
+            
             if l1:
-                curr.next = l1
+                dummy.next = l1
+            
             if l2:
-                curr.next = l2
-            return dummy.next
+                dummy.next = l2
+
+            return curr.next
 
         while len(lists) > 1:
             mergedList = []
-            for i in range(0, len(lists), 2):                
-                l1 = lists[i]
-                l2 = None
-                if i + 1 <= (len(lists) - 1):
-                    l2 = lists[i + 1]
-                mergedList.append(combineLinkedLists(l1, l2))
+            for i in range(0, len(lists), 2):
+                # print(i)
+                list1 = lists[i]
+                if i + 1 >= len(lists):
+                    list2 = None
+                else:
+                    list2 = lists[i + 1]
+                newList = mergeList(list1, list2)
+                # print(newList)
+                mergedList.append(newList)
+            # print(mergedList)
             lists = mergedList
+            # print(lists)
+        if lists:
+            return lists[0]
+        else:
+            return None
 
-        return (lists[0])
-
+                
