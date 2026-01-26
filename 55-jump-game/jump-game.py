5f9c3b3727ec8967131122040b727112bc23f015
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-01-26 17:38:06 +0530

    Time: 31 ms (62.95%) | Memory: 13.3 MB (56.07%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -3,13 +3,10 @@ class Solution(object):
         """
         :type nums: List[int]
         :rtype: bool
-        """
+        """ 
         maxIndex = 0
         for i in range(len(nums)):
             if i > maxIndex:
                 return False
             maxIndex = max(maxIndex, i + nums[i])
-            if maxIndex >= (len(nums) - 1):
-                return True
-        
-        
+        return True
