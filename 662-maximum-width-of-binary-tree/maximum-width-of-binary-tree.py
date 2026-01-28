4f6c0ae08bcd8a85a223cfe5087baa450c8c6a3d
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-01-28 17:35:49 +0530

    Time: 7 ms (54.83%) | Memory: 14 MB (66.06%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.py
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.py
@@ -10,23 +10,27 @@ class Solution(object):
         :type root: Optional[TreeNode]
         :rtype: int
         """
-        
         queue = deque([(root, 0)])
         maxWidth = 0
         while queue:
             size = len(queue)
             first = -1
             last = -1
+            min_val = -1
             for i in range(size):
-                node, id = queue.popleft()
+                root, ind = queue.popleft()
                 if i == 0:
-                    first = id
+                    first = ind
+                
                 if i == size - 1:
-                    last = id
-                if node.left:
-                    queue.append((node.left, 2 * id + 1))
-                if node.right:
-                    queue.append((node.right, 2 * id + 2))
-            maxWidth = max(maxWidth, last - first + 1)
-        return maxWidth
+                    last = ind
 
+                if root.left:
+                    queue.append((root.left, (ind - first) * 2 + 1))
+
+                if root.right:
+                    queue.append((root.right, (ind - first) * 2 + 2))
+            
+            maxWidth = max(maxWidth, (last - first + 1))
+
+        return maxWidth
