c32c8c1fd7dc82279057bbc2c60c39a096e328d4
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-01-28 17:53:12 +0530

    Time: 11 ms (98.48%) | Memory: 12.7 MB (65.05%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.py
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.py
@@ -13,42 +13,50 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
+        self.lookingFor = -1
         parentMap = {}
-        queue = deque([root])
-        while queue:
-            # print(queue)
-            node = queue.popleft()
-            if node.left:
-                parentMap[node.left] = node
-                queue.append(node.left)
-            if node.right:
-                parentMap[node.right] = node
-                queue.append(node.right)
+        def preOrder(root):
+            if root == None:
+                return None
+            
+            if root.val == target.val:
+                self.lookingFor = root
+            
+            if root.right:
+                parentMap[root.right] = root
+            
+            if root.left:
+                parentMap[root.left] = root
+            
+            preOrder(root.left)
+            preOrder(root.right)
+        
+        preOrder(root)
 
-        queue = deque([target])
-        dist = 0
-        visited = set([target])
-        while queue:
+        # print([[key.val, parentMap[key].val] for key in parentMap])
 
-            if dist == k:
+        queue = deque([self.lookingFor])
+        visited = set([self.lookingFor])
+        c = 0
+
+        while queue:
+            if c == k:
                 break
-            dist += 1
+            c += 1 
 
             size = len(queue)
             for i in range(size):
                 node = queue.popleft()
+                if node.right and node.right not in visited:
+                    queue.append(node.right)
 
                 if node.left and node.left not in visited:
                     queue.append(node.left)
-                    visited.add(node.left)
 
                 if node in parentMap and parentMap[node] not in visited:
                     queue.append(parentMap[node])
-                    visited.add(parentMap[node])
-                    # print(node.val, parentMap[node].val)
-
-                if node.right and node.right not in visited:
-                    queue.append(node.right)
-                    visited.add(node.right)
+                
+                visited.add(node)
         
-        return list([node.val for node in queue])
+        return [x.val for x in queue]
+                
