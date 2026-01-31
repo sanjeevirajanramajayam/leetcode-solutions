2d17c676f18a6d167b02072a4a7fb9a64b50110e
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-01-31 18:09:49 +0530

    Time: 301 ms (5.72%) | Memory: 13.7 MB (37.24%) - LeetSync

---
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def nextGreatestLetter(self, letters, target):
+        """
+        :type letters: List[str]
+        :type target: str
+        :rtype: str
+        """
+        found = False
+        compChar = '0'
+        for i in letters:
+            if found == False:
+                print(i, target, i < target)
+                if i > target:
+                    compChar = i
+                    found = True
+            else:
+                if i < compChar:
+                    compChar = i
+        if compChar == '0':
+            return letters[0]
+        return compChar
