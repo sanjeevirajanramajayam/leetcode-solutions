282fd26c291610fcaf5fc7b9426bec433b7ef29d
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-02-01 16:09:51 +0530

    Time: 7 ms (76.70%) | Memory: 13.4 MB (46.77%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def findCircleNum(self, isConnected):
+        """
+        :type isConnected: List[List[int]]
+        :rtype: int
+        """
+
+        adjList = [[] for _ in range(len(isConnected) + 1)]
+
+        for i in range(len(isConnected)):
+            for j in range(len(isConnected)):
+                if isConnected[i][j] == 1:
+                    if i != j:
+                        adjList[i + 1].append(j + 1)
+        
+
+
+        count = 0
+        visited = [-1 for _ in range(len(isConnected) + 1)]
+
+        def dfs(node):
+            visited[node] = 1
+            # print(node)
+            # print(node, adjList[node])
+            for i in adjList[node]:
+                if visited[i] == -1: 
+                    dfs(i)
+                    print(i)
+
+        for i in range(1, len(isConnected) + 1):
+            if visited[i] == -1:
+                dfs(i)
+                # print(i)
+                count += 1
+        
+        return count
