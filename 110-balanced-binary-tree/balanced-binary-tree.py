425589ad4f9f984fd32f2ba46dbecc102f17b310
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-02-08 20:39:08 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (56.44%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.py
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -6,17 +6,24 @@
 #         self.right = right
 class Solution(object):
     def isBalanced(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: bool
+        """
+        
         def maxH(root):
             if root == None:
                 return 0
-
+            
             lh = maxH(root.left)
             rh = maxH(root.right)
-
+            
             if lh == -1 or rh == -1:
                 return -1
+
             if abs(lh - rh) > 1:
                 return -1
-
-            return 1 + max(lh, rh)
+            
+            return max(lh, rh) + 1
+        
         return maxH(root) != -1
