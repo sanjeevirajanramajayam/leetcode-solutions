f95ed2948db358e6be06e2b3c529c10047bebb0b
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-02-14 09:25:30 +0530

    Time: 176 ms (6.90%) | Memory: 12.5 MB (58.62%) - LeetSync

---
+++ 815-champagne-tower/champagne-tower.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def champagneTower(self, poured, query_row, query_glass):
+        """
+        :type poured: int
+        :type query_row: int
+        :type query_glass: int
+        :rtype: float
+        """
+        glasses = [[0 for i in range(0 , j + 1)] for j in range(0, query_row + 1)]
+        glasses[0][0] = poured
+        # print(glasses)
+        for i in range(0, query_row):
+            for j in range(0, i + 1):
+                quantityLeft = (glasses[i][j] - 1) / 2.0
+                if quantityLeft <= 0:
+                    continue
+                print(quantityLeft)
+                glasses[i + 1][j] += quantityLeft
+                glasses[i + 1][j + 1] += quantityLeft
+        # print(glasses)
+        return min(1, glasses[query_row][query_glass])
