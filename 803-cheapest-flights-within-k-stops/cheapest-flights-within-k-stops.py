12b3a0a63250bc23c5a38ba99ab05ca6b0059600
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-02-17 10:45:19 +0530

    Time: 6 ms (81.75%) | Memory: 13.5 MB (47.72%) - LeetSync

---
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def findCheapestPrice(self, n, flights, src, dst, k):
+        """
+        :type n: int
+        :type flights: List[List[int]]
+        :type src: int
+        :type dst: int
+        :type k: int
+        :rtype: int
+        """
+
+        adjList = [[] for i in range(n)]
+
+        for startNode, endNode, cost in flights:
+            adjList[startNode].append((endNode, cost))
+
+        pq = deque([(0, src, 0)])
+        distArray = [float('inf') for i in range(n)]
+        # distArray[0] = 0
+
+        while pq:
+            stops, node, dist = pq.popleft()
+            if node == dst:
+                continue
+            for newNode, edW in adjList[node]:
+                if edW + dist < distArray[newNode] and stops <= k:
+                    distArray[newNode] = edW + dist
+                    pq.append((stops + 1, newNode, distArray[newNode]))
+        if distArray[dst] == float('inf'):
+            return -1
+        return distArray[dst]
