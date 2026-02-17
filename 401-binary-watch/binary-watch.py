9d58ccee377147ed031ddd5b4ce9d65a09bdfa78
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-02-17 14:23:09 +0530

    Time: 7 ms (4.93%) | Memory: 19.5 MB (39.62%) - LeetSync

--- 401-binary-watch/binary-watch.py
+++ 401-binary-watch/binary-watch.py
@@ -2,24 +2,25 @@ class Solution:
     def readBinaryWatch(self, turnedOn: int) -> List[str]:
         arr = ["0"] * 10
         res = []
+
         def watch(ind, temp, k):
-            hour = int("".join(temp[:4]), 2)
-            minute = int("".join(temp[4:]), 2)
-            if hour > 11:
-                return
-            if minute >= 60:
-                return
+
             if ind == len(temp):
+                hour = int("".join(temp[:4]), 2)
+                minute = int("".join(temp[4:]), 2)
+                if hour > 11:
+                    return
+                if minute >= 60:
+                    return
                 if sum(map(int, temp)) == turnedOn:
                     timeString = f"{hour}:{minute:02}"
-                    # print(timeString)
                     res.append(timeString)
                 return
             if k > 0:
                 temp[ind] = "1"
-                watch(ind + 1, temp, k - 1) 
+                watch(ind + 1, temp, k - 1)
             temp[ind] = "0"
             watch(ind + 1, temp, k)
-    
+
         watch(0, arr, turnedOn)
         return res
