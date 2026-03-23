5284def27ccb129693a612c534dca50287a51cc6
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-03-23 21:27:18 +0530

    Time: 4 ms (21.50%) | Memory: 19.3 MB (55.61%) - LeetSync

--- 2053-check-if-all-characters-have-equal-number-of-occurrences/check-if-all-characters-have-equal-number-of-occurrences.py
+++ 2053-check-if-all-characters-have-equal-number-of-occurrences/check-if-all-characters-have-equal-number-of-occurrences.py
@@ -3,7 +3,7 @@ class Solution:
         hashmap = {}
         for i in range(len(s)):
             hashmap[s[i]] = hashmap.get(s[i], 0) + 1
-        print(hashmap.values())
+        # print(hashmap.values())
         if len(set(hashmap.values())) == 1:
             return True
         return False
