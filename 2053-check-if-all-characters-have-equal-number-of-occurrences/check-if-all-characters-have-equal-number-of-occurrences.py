a07c9197724b98986abc6640493e7795ad5b0979
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-03-23 21:27:13 +0530

    Time: 3 ms (64.16%) | Memory: 19.4 MB (19.25%) - LeetSync

---
+++ 2053-check-if-all-characters-have-equal-number-of-occurrences/check-if-all-characters-have-equal-number-of-occurrences.py
@@ -0,0 +1,9 @@
+class Solution:
+    def areOccurrencesEqual(self, s: str) -> bool:
+        hashmap = {}
+        for i in range(len(s)):
+            hashmap[s[i]] = hashmap.get(s[i], 0) + 1
+        print(hashmap.values())
+        if len(set(hashmap.values())) == 1:
+            return True
+        return False
