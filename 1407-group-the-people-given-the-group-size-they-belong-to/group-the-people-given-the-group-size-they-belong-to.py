f608dd667f22b49f9e4b280e62d27c8c14156efd
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-03-24 15:30:37 +0530

    Time: 15 ms (7.59%) | Memory: 19.5 MB (12.77%) - LeetSync

---
+++ 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.py
@@ -0,0 +1,16 @@
+class Solution:
+    def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
+        hash = {}
+        for person in range(len(groupSizes)):
+            if groupSizes[person] not in hash:
+                hash[groupSizes[person]] = [[]]
+            print(len(hash[groupSizes[person]][-1]), groupSizes[person])
+            if len(hash[groupSizes[person]][-1]) >= groupSizes[person]:
+                hash[groupSizes[person]].append([])
+            hash[groupSizes[person]][-1].append(person)
+        ans = []
+
+        for key in hash:
+            ans += hash[key]
+        
+        return ans
