aff81c7ab4969d7e8914f8358147e68df381d78c
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-03-24 21:23:33 +0530

    Time: 422 ms (36.99%) | Memory: 48 MB (91.44%) - LeetSync

--- 1364-tuple-with-same-product/tuple-with-same-product.py
+++ 1364-tuple-with-same-product/tuple-with-same-product.py
@@ -2,12 +2,12 @@ class Solution:
     def tupleSameProduct(self, nums: List[int]) -> int:
         hash = {}
         for i in range(len(nums)):
-            for j in range(len(nums)):
+            for j in range(i + 1, len(nums)):
                 if nums[i] != nums[j]:
                     hash[nums[i] * nums[j]] = hash.get(nums[i] * nums[j], 0) + 1
                     # print(nums[i], nums[j], nums[i] * nums[j])
         # print(hash)
         cnt = 0
         for prod in hash:
-            cnt += (hash[prod] // 2) * (hash[prod] // 2 - 1)
+            cnt += (hash[prod]) * (hash[prod] - 1)
         return cnt * 4
