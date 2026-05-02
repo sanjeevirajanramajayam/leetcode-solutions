118c10d378f7c3702589adfb1b5df8c90a0da701
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-05-02 17:31:55 +0530

    Time: 11 ms (25.72%) | Memory: 20.2 MB (59.38%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -1,23 +1,28 @@
 class Solution:
-    def setZeroes(self, matrix: List[List[int]]) -> None:
+    def setZeroes(self, mat: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        colSet = set()
-        rowSet = set()
-
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                if matrix[i][j] == 0:
-                    colSet.add(j)
-                    rowSet.add(i)
-
-        for i in rowSet:
-            for j in range(len(matrix[0])):
-                # print(i, j)
-                matrix[i][j] = 0
-
-        for i in range(len(matrix)):
-            for j in colSet:
-                matrix[i][j] = 0
-         
+        col1 = mat[0][0]
+        for i in range(len(mat)):
+            for j in range(len(mat[0])):
+                if mat[i][j] == 0:
+                    if j == 0:
+                        col1 = 0
+                    else:
+                        mat[0][j] = 0
+                    mat[i][0] = 0
+        
+        for i in range(1, len(mat)):
+            for j in range(1, len(mat[0])):
+                if mat[0][j] == 0 or mat[i][0] == 0:
+                    mat[i][j] = 0
+        
+        for i in range(len(mat[0]) - 1, -1, -1):
+            if mat[0][0] == 0 or mat[0][i] == 0:
+                mat[0][i] = 0
+        
+        for i in range(len(mat)):
+            if mat[i][0] == 0 or col1 == 0:
+                mat[i][0] = 0
+        
