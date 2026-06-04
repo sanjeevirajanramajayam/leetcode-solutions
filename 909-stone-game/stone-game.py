a7204629e4856a31682fd79a29d23d58522699ec
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-06-04 13:32:00 +0530

    Time: 448 ms (9.63%) | Memory: 151 MB (5.13%) - LeetSync

---
+++ 909-stone-game/stone-game.py
@@ -0,0 +1,18 @@
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        hashmap = {}
+        def fn(i, j, turn):
+            if i > j:
+                return 0
+            if (i, j, turn) in hashmap:
+                return hashmap[(i, j, turn)]
+            if turn == 0:
+                first = piles[i] + fn(i + 1, j, 1)
+                last = piles[j] + fn(i, j - 1, 1)
+            else:
+                first = -piles[i] + fn(i + 1, j, 0)
+                last = -piles[j] + fn(i, j - 1, 0)
+            # print(i, j, turn, first, last)
+            hashmap[(i, j, turn)] = max(first, last)
+            return max(first, last)
+        return True if fn(0, len(piles) - 1, 0) > 0 else False
