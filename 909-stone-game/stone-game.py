b95b81d85a544361f38ef1bf0c39328076e80450
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-06-04 13:56:45 +0530

    Time: 895 ms (5.01%) | Memory: 60.6 MB (26.49%) - LeetSync

--- 909-stone-game/stone-game.py
+++ 909-stone-game/stone-game.py
@@ -15,4 +15,28 @@ class Solution:
             # print(i, j, turn, first, last)
             hashmap[(i, j, turn)] = max(first, last)
             return max(first, last)
-        return True if fn(0, len(piles) - 1, 0) > 0 else False
+
+        dp = [[[-1 for _ in range(2)] for _ in range((len(piles) + 1))] for i in range(len(piles) + 1)]
+
+        for i in range(len(piles)):
+            for j in range(len(piles)):
+                if i > j:
+                    dp[i][j][0] = 0
+                    dp[i][j][1] = 0
+
+        for i in range(len(piles) - 1, -1, -1):
+            for j in range(len(piles)):
+                # if i > j:
+                #     dp[i][j][0] = 0
+                #     dp[i][j][1] = 0
+                for turn in range(2):
+                    if turn == 0:
+                        # print(i, j)
+                        first = piles[i] + dp[i + 1][j][1]
+                        last = piles[j] + dp[i][j - 1][1]
+                    else:
+                        first = -piles[i] + dp[i + 1][j][0]
+                        last = -piles[j] + dp[i][j - 1][0]
+                    dp[i][j][turn] = max(first, last)
+                    # print(i, j, turn, first, last)
+        return True if dp[0][len(piles) - 1][0] > 0 else False
