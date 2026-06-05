cc8bf872884c630e28fa5a7e2fa2558ff09a3d37
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-06-05 13:09:46 +0530

    Time: 3 ms (32.82%) | Memory: 19.4 MB (66.25%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,14 +1,12 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
+        visited = [0] * len(nums)
         ans = []
-        visited = [0 for i in range(len(nums))]
         def fn(ind, arr):
-            # print(ind, arr)
             if ind == len(nums):
                 ans.append(arr[:])
                 return 
-
-            for i in range(len(nums)):
+            for i in range(len(visited)):
                 if visited[i] == 0:
                     visited[i] = 1
                     fn(ind + 1, arr + [nums[i]])
