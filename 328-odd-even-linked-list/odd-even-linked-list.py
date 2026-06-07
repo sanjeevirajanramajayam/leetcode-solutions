22b76f1f267cd407b723156566721f07b270e9f8
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-06-07 10:11:44 +0530

    Time: 4 ms (13.16%) | Memory: 15.6 MB (99.59%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.py
+++ 328-odd-even-linked-list/odd-even-linked-list.py
@@ -9,35 +9,17 @@ class Solution(object):
         :type head: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        oddHead = None
-        evenHead = None
-        evenB = None
-        oddB = None
-        curr = head
-        cnt = 1
-        if not head:
-            return None
-        if not head.next:
+        if not head or not head.next:
             return head
-        while curr:
-            front = curr.next
-            if cnt % 2 == 0:
-                if evenHead == None:
-                    evenHead = curr
-                    evenB = curr
-                else:
-                    evenHead.next = curr
-                    evenHead = evenHead.next
-            else:
-                if oddHead == None:
-                    oddHead = curr
-                    oddB = curr
-                else:
-                    oddHead.next = curr
-                    oddHead = oddHead.next
-            cnt += 1
-            curr = front
-        oddHead.next = None
-        evenHead.next = None
-        oddHead.next = evenB
-        return oddB
+        
+        odd = head
+        even = head.next
+        evenHead = even
+
+        while even and even.next: # even is always forward
+            odd.next = odd.next.next
+            even.next = even.next.next
+            odd = odd.next
+            even = even.next
+        odd.next = evenHead
+        return head
