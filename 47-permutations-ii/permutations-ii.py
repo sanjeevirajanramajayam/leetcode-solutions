32c2f3b92639fe19f3ff6f853e75dbf8978245d3
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-06-12 21:49:50 +0530

    Time: 5 ms (49.30%) | Memory: 19.9 MB (22.88%) - LeetSync

--- 47-permutations-ii/permutations-ii.py
+++ 47-permutations-ii/permutations-ii.py
@@ -1,15 +1,20 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        visited = [0] * len(nums)
-        ans = set()
-        def fn(ind, arr):
-            if ind == len(nums):
-                ans.add(tuple(arr[:]))
+        freqMap = {}
+        for i in range(len(nums)):
+            freqMap[nums[i]] = freqMap.get(nums[i], 0) + 1
+        ans = []
+        def fn(mapd, arr):
+            if len(arr) == len(nums):
+                ans.append(arr[:])
                 return 
-            for i in range(len(visited)):
-                if visited[i] == 0:
-                    visited[i] = 1
-                    fn(ind + 1, arr + [nums[i]])
-                    visited[i] = 0
-        fn(0, [])
-        return [list(x) for x in ans]
+        
+            for i in mapd:
+                if mapd[i] != 0:
+                    mapd[i] = mapd.get(i, 0) - 1
+                    arr.append(i)
+                    fn(mapd, arr)
+                    mapd[i] = mapd.get(i, 0) + 1
+                    arr.pop()
+        fn(freqMap, [])
+        return ans
