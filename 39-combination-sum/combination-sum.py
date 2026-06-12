b324edae16392965c093ae50d637f1817796c25b
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-06-12 21:38:01 +0530

    Time: 11 ms (87.98%) | Memory: 12.4 MB (56.43%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,19 +1,18 @@
-class Solution:
-    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        candidates.sort()
-        ans = []
-        def fn(i, sum, a):
-            if i == 0:
-                # print(target, sum)
-                while sum != target and (target - sum) % candidates[i] == 0:
-                    a.append(candidates[i])
-                    sum += candidates[i]
-                # print(sum, target)
-                if sum == target:
-                    ans.append(a[:])
-                return
-            if sum + candidates[i] <= target:
-                take = fn(i, sum + candidates[i], a + [candidates[i]])
-            not_take = fn(i - 1, sum , a)
-        fn(len(candidates) - 1, 0 ,[])
+class Solution(object):
+    def combinationSum(self, nums, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        ans=[]
+        def fn(ind, currSum, arr):
+            if currSum == target:
+                ans.append(arr[:])
+                return True
+            
+            for i in range(ind, len(nums)):
+                if currSum + nums[i] <= target:
+                    fn(i, currSum + nums[i], arr + [nums[i]])
+        fn(0, 0, [])
         return ans
