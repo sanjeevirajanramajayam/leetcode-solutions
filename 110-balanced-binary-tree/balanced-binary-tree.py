e346575fe1102610efb9c25487678e5975eaa110
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-06-17 07:31:27 +0530

    Time: 0 ms (100.00%) | Memory: 20.3 MB (70.57%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.py
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -1,29 +1,26 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def isBalanced(self, root):
-        """
-        :type root: Optional[TreeNode]
-        :rtype: bool
-        """
-        
-        def maxH(root):
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def preorder(root):
             if root == None:
                 return 0
             
-            lh = maxH(root.left)
-            rh = maxH(root.right)
-            
-            if lh == -1 or rh == -1:
-                return -1
+            left = preorder(root.left)
+            right = preorder(root.right)
 
-            if abs(lh - rh) > 1:
-                return -1
+            if left is False or right is False:
+                return False
+
+            left += 1
+            right += 1
+
+            if abs(left - right) > 1:
+                return False
             
-            return max(lh, rh) + 1
-        
-        return maxH(root) != -1
+            return max(left, right)
+        return preorder(root) is not False
