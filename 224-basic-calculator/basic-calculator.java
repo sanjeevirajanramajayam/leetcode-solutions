97af8c9f861739513cd54f3e7142fc2e6cc5dbbe
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-07-05 16:57:53 +0530

    Time: 14 ms (21.13%) | Memory: 46.6 MB (42.46%) - LeetSync

--- 224-basic-calculator/basic-calculator.java
+++ 224-basic-calculator/basic-calculator.java
@@ -1,39 +1,46 @@
 class Solution {
     public int calculate(String s) {
-        int currentNumber = 0;
-        int sign = 1;
+        int currentNum = 0;
         int result = 0;
-        Stack<Integer> stack = new Stack<>();
+        int sign = 1;
+        Stack<Integer> st = new Stack<>();
 
-        for (char c : s.toCharArray()) {
+        for (Character c: s.toCharArray()) {
             if (Character.isDigit(c)) {
-                currentNumber = (currentNumber * 10) + (c - 48);
+                currentNum = currentNum * 10;
+                currentNum += (c - '0');
+                continue;
             }
-            else if (c == '+' || c == '-') {
-                result += (currentNumber * sign);
-                if (c == '-') {
-                    sign = -1;
-                }
-                else {
-                    sign = 1;
-                }
-                currentNumber = 0;
+            if (c == ' ') {
+                continue;
+            }
+            if ( c == '+') {
+                result += (currentNum * sign);
+                currentNum = 0;
+                sign = 1;
+            }
+            else if (c == '-') {
+                result += (currentNum * sign);
+                sign = -1;
+                currentNum = 0;
             }
             else if (c == '(') {
-                stack.push(result);
-                stack.push(sign);
+                st.push(result);
+                st.push(sign);
+
+                currentNum = 0;
                 result = 0;
                 sign = 1;
             }
             else if (c == ')') {
-                result += (currentNumber * sign);
-                currentNumber = 0;
-                sign = 1;
-                result *= stack.pop();
-                result += stack.pop();
+                result += currentNum * sign;
+                result *= st.pop();
+                result += st.pop();
+                currentNum = 0;
             }
+
         }
-        result += (currentNumber * sign);
+        result += currentNum * sign;
         return result;
     }
 }
