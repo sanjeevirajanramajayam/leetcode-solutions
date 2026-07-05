def2140de0753f48af120d0d53550f27727cda9c
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-07-05 14:13:01 +0530

    Time: 3 ms (31.20%) | Memory: 119.7 MB (9.62%) - LeetSync

--- 134-gas-station/gas-station.java
+++ 134-gas-station/gas-station.java
@@ -1,22 +1,28 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        // int arr[] = new int[gas.length];
-        // for (int i = 0; i < gas.length; i++) {
-        //     arr[i] = gas[i] - cost[i];
-        // }
+        int[] arr = new int[gas.length];
+        int n = gas.length;
+        int gasT = 0;
+        int costT = 0;
+        for (int i = 0; i < n; i++) {
+            arr[i] = gas[i] - cost[i];
+            gasT += gas[i];
+            costT += cost[i];
+        }
+        if (costT > gasT) {
+            return -1;
+        }
         int sum = 0;
         int start = 0;
-        int nSum = 0;
-        for (int i = 0; i < gas.length; i++) {
-            if (sum == 0)
+        for (int i = 0; i < n; i++) {
+            if (sum == 0) {
                 start = i;
-            sum += gas[i] - cost[i];
-            nSum += gas[i] - cost[i];
-            if (sum < 0)
+            }
+            sum += arr[i];
+            if (sum < 0) {
                 sum = 0;
+            }
         }
-        if (nSum < 0)
-            return -1;
-        return (start);
+        return start;
     }
 }
