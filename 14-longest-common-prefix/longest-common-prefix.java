463580963ee5597dba54f5e603316b459b039e50
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-07-05 16:37:47 +0530

    Time: 2 ms (17.87%) | Memory: 43.5 MB (8.28%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,25 +1,59 @@
+class Trie {
+    TrieNode root;
+
+    Trie() {
+        this.root = new TrieNode();
+    }
+
+    void insert(String s) {
+        TrieNode curr = this.root;
+        for (Character c : s.toCharArray()) {
+            if (curr.children[c - 'a'] == null) {
+                curr.children[c - 'a'] = new TrieNode();
+            }
+            curr = curr.children[c - 'a'];
+        }
+        // return True;
+        curr.isEnd = true;
+    }
+
+    String search() {
+        TrieNode curr = this.root;
+        StringBuilder sb = new StringBuilder();
+        while (!curr.isEnd && cntChildren(curr) == 1) {
+            for (int i = 0; i < curr.children.length; i++) {
+                if (curr.children[i] != null) {
+                    sb.append((char)('a' + i));
+                    curr = curr.children[i];
+                    break;
+                }
+            }
+        }
+        return sb.toString();
+    }
+
+    int cntChildren (TrieNode root) {
+        int cnt = 0;
+        for (TrieNode x : root.children) {
+            if (x != null) {
+                cnt += 1;
+            }
+        }
+        return cnt;
+    }
+}
+
+class TrieNode {
+    TrieNode[] children = new TrieNode[26];
+    boolean isEnd = false;
+}
+
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        StringBuilder ans = new StringBuilder("");
-        Character currentChar = 'A';
-        int minLen = Integer.MAX_VALUE;
-        for (int i = 0; i < strs.length; i++) {
-            minLen = Math.min(minLen, strs[i].length());
+        Trie t = new Trie();
+        for (String s: strs) {
+            t.insert(s);
         }
-
-        for (int i = 0; i < minLen; i++) {
-            for (int j = 0; j < strs.length; j++) {
-                if (currentChar == 'A') {
-                    currentChar = strs[j].charAt(i);
-                }
-                if (strs[j].charAt(i) != currentChar) {
-                    return ans.toString();
-                } 
-            }
-            ans.append(currentChar);
-            currentChar = 'A';
-        }
-
-        return ans.toString();
+        return t.search();
     }
 }
