fdaac27ef69e082c0ac38db129b3748213b42623
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-07-06 12:26:13 +0530

    Time: 4 ms (75.24%) | Memory: 44.4 MB (69.02%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.java
+++ 257-binary-tree-paths/binary-tree-paths.java
@@ -14,26 +14,21 @@
  * }
  */
 class Solution {
-    List<String> ans = new ArrayList<>();
-    void fn(TreeNode root, StringBuilder temp) {
+    List<String> a = new ArrayList<>();
+    public void build(TreeNode root, StringBuilder s) {
         if (root == null) {
             return;
         }
-        StringBuilder curr = new StringBuilder(temp);
-
+        StringBuilder curr = new StringBuilder(s);
         if (root.left == null && root.right == null) {
-            curr.append(Integer.toString(root.val));
-            this.ans.add(new StringBuilder(curr).toString());
+            this.a.add(curr.append(Integer.toString(root.val)).toString());
+            return;
         }
-        else {
-            curr.append(Integer.toString(root.val)+"->");
-        }
-        fn( root.left,  curr);
-        fn( root.right,  curr);
-
+        build(root.left, new StringBuilder(curr).append(Integer.toString(root.val) + "->"));
+        build(root.right, new StringBuilder(curr).append(Integer.toString(root.val) + "->"));
     }
     public List<String> binaryTreePaths(TreeNode root) {
-        fn(root, new StringBuilder());
-        return this.ans;
+        build(root, new StringBuilder());
+        return a;
     }
 }
