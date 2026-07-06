8ad232ed75249443ceac5d793afba69122b72c3a
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-07-06 10:48:34 +0530

    Time: 13 ms (26.04%) | Memory: 46.5 MB (58.13%) - LeetSync

--- 224-basic-calculator/basic-calculator.java
+++ 224-basic-calculator/basic-calculator.java
@@ -2,45 +2,46 @@ class Solution {
     public int calculate(String s) {
         int currentNum = 0;
         int result = 0;
-        int sign = 1;
+        int sign = 1; // old sign value
         Stack<Integer> st = new Stack<>();
 
         for (Character c: s.toCharArray()) {
-            if (Character.isDigit(c)) {
-                currentNum = currentNum * 10;
-                currentNum += (c - '0');
-                continue;
-            }
             if (c == ' ') {
                 continue;
             }
-            if ( c == '+') {
-                result += (currentNum * sign);
-                currentNum = 0;
+            if (Character.isDigit(c)) {
+                currentNum  = currentNum * 10 + (c - 48);
+                continue;
+            }
+            if (c == '+') {
+                result += currentNum * sign; // use old sign
                 sign = 1;
             }
             else if (c == '-') {
-                result += (currentNum * sign);
+                result += currentNum * sign;
                 sign = -1;
-                currentNum = 0;
             }
             else if (c == '(') {
                 st.push(result);
                 st.push(sign);
-
-                currentNum = 0;
                 result = 0;
+                // currenNum = 0;
                 sign = 1;
             }
             else if (c == ')') {
                 result += currentNum * sign;
                 result *= st.pop();
                 result += st.pop();
-                currentNum = 0;
             }
-
+            currentNum = 0;
         }
-        result += currentNum * sign;
+        // System.out.println(currentNum * sign + " " + st);
+        result += (currentNum * sign);
+        // System.out.println(currentNum * sign + " " + st);
+        // int sum = 0;
+        // for (int x : st) {
+        //     sum += x;
+        // }
         return result;
     }
 }
