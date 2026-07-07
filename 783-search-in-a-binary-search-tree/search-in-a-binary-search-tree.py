f331789878fdac4e62d3051585b3a28726ad3f16
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-07-07 18:44:37 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (35.16%) - LeetSync

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.py
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.py
@@ -1,24 +1,23 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
-        def traverse(root):
-            if root == None:
-                return None
-            # print(root.val)
+class Solution(object):
+    def searchBST(self, root, val):
+        """
+        :type root: Optional[TreeNode]
+        :type val: int
+        :rtype: Optional[TreeNode]
+        """
+        def search(root):
+            if not root:
+                return
             if root.val == val:
                 return root
-            
-            if val > root.val:
-                x = traverse(root.right) 
-                if x is not None:
-                    return x
+            if (val > root.val):
+                return search(root.right)
             else:
-                x = traverse(root.left)
-                if x is not None:
-                    return x 
-        return traverse(root)
+                return search(root.left)
+        return search(root)
