b532648086ab831db99fe5e1a5a63bf78c46649b
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-07-10 14:52:40 +0530

    Time: 69 ms (31.60%) | Memory: 22 MB (82.78%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -1,27 +1,43 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
 
-class Solution(object):
-    def lowestCommonAncestor(self, root, p, q):
-        """
-        :type root: TreeNode
-        :type p: TreeNode
-        :type q: TreeNode
-        :rtype: TreeNode
-        """
-        def fn(root, lb, rb):
-            target = root.val
-            if p.val < target and q.val < target:
-                return fn(root.left, lb, root.val)
-            elif p.val > target and q.val > target:
-                return fn(root.right, root.val, rb)
-            else:
-                return root
-        return fn(root, float('-inf'), float('inf'))
-            
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        def lca(root):
+            if root == None:
+                return
 
+                
+            if root.val == p.val:
+                return root
             
+            if root.val == q.val:
+                return root
+
+
+            left = lca(root.left)
+            right = lca(root.right)
+            # print(root.val)
+            # if left:
+            #     print(left.val)
+            # else:
+            #     print("None")
+            # if right:
+            #     print(right.val)
+            # else:
+            #     print("None")
+
+            if left and right:
+                return root
+
+            if left:
+                return left
+            
+            if right:
+                return right
+        
+        return lca(root)
