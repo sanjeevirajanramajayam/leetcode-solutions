e74850e89e68d76afc5f0c75fc966932f25512fa
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-07-10 14:21:52 +0530

    Time: 0 ms (100.00%) | Memory: 25.5 MB (91.15%) - LeetSync

--- 173-binary-search-tree-iterator/binary-search-tree-iterator.py
+++ 173-binary-search-tree-iterator/binary-search-tree-iterator.py
@@ -1,42 +1,29 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class BSTIterator(object):
+class BSTIterator:
 
-    def __init__(self, root):
-        """
-        :type root: Optional[TreeNode]
-        """
+    def __init__(self, root: Optional[TreeNode]):
         self.stack = []
         curr = root
         while curr:
             self.stack.append(curr)
             curr = curr.left
-        # print([x.val for x in self.stack])
 
-    def next(self):
-        """
-        :rtype: int
-        """
-        currNode = self.stack.pop()
-        curr = currNode.right
-        if curr:
+    def next(self) -> int:
+        node = self.stack.pop()
+        if node.right:
+            curr = node.right
             self.stack.append(curr)
-        while curr and curr.left:
-            self.stack.append(curr.left)
-            curr = curr.left
-        # print([x.val for x in self.stack])
-        return currNode.val
-        
-            
+            while curr.left:
+                self.stack.append(curr.left)
+                curr = curr.left
+        return node.val
 
-    def hasNext(self):
-        """
-        :rtype: bool
-        """
+    def hasNext(self) -> bool:
         return self.stack != []
 
 
