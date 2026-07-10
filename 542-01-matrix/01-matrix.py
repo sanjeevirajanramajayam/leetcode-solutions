97fcc4855f806d8d082d5912dfdd48803fa240d5
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-07-10 19:59:29 +0530

    Time: 131 ms (43.85%) | Memory: 22.2 MB (57.74%) - LeetSync

--- 542-01-matrix/01-matrix.py
+++ 542-01-matrix/01-matrix.py
@@ -1,40 +1,29 @@
-class Solution(object):
-    def updateMatrix(self, mat):
-        """
-        :type mat: List[List[int]]
-        :rtype: List[List[int]]
-        """
-        
-        ROWS = len(mat)
-        COLS = len(mat[0])
-
-        ans = [[0 for _ in range(COLS)] for _ in range(ROWS)]
-
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        ROWS = len(mat) 
+        COLS = len(mat[0]) 
         queue = deque([])
-        visited = set([])
-
-        for i in range(ROWS):
-            for j in range(COLS):
+        visited = set()
+        directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+        for i in range(len(mat)):
+            for j in range(len(mat[0])):
                 if mat[i][j] == 0:
-                    queue.append((i, j, 0))
+                    queue.append((i, j))
                     visited.add((i, j))
-
-        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-        distance = 0
-        
+        time = 1
         while queue:
-            distance += 1
-            size = len(queue)
-            for i in range(size):
-                cRow, cCol, dist = queue.popleft()
-                ans[cRow][cCol] = dist
-                
-                found = False
-                for dr, dc in directions:
-                    uRow, uCol = cRow + dr, cCol + dc
-                    if uRow >= 0 and uRow < ROWS and uCol >= 0 and uCol < COLS:
-                        if (uRow, uCol) not in visited:
-                            queue.append((uRow, uCol, dist + 1))
-                            visited.add((uRow, uCol))
-
-        return ans
+            # print(queue)
+            qlen = len(queue)
+            for _ in range(qlen):
+                i, j = queue.popleft()
+                for dx, dy in directions:
+                    nx, ny = i + dx, j + dy
+                    # print(visited)
+                    if nx >= 0 and nx < ROWS and ny >= 0 and ny < COLS:
+                        if (nx, ny) not in visited:
+                            if mat[nx][ny] == 1:
+                                mat[nx][ny] = time
+                                queue.append((nx, ny))
+                                visited.add((nx, ny))
+            time += 1
+        return mat
