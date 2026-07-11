402d526217c41efbb08e012c2c56524b8bc57c0e
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-07-11 18:55:00 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (65.25%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        hashmap = {')' : '(', '}':'{', ']':'['}
+        for i in s:
+            if i not in hashmap:
+                stack.append(i)
+            elif stack and hashmap[i] == stack[-1]:
+                stack.pop()
+            else:
+                return False
+            
+        return len(stack) == 0
