522e981c39a8cda026b84ab902410c9a671a9396
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-07-13 18:05:20 +0530

    Time: 145 ms (32.54%) | Memory: 22.3 MB (46.99%) - LeetSync

--- 542-01-matrix/01-matrix.py
+++ 542-01-matrix/01-matrix.py
@@ -1,29 +1,29 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        ROWS = len(mat) 
-        COLS = len(mat[0]) 
+        ROWS = len(mat)
+        COLS = len(mat[0])
         queue = deque([])
         visited = set()
-        directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
         for i in range(len(mat)):
             for j in range(len(mat[0])):
                 if mat[i][j] == 0:
                     queue.append((i, j))
                     visited.add((i, j))
-        time = 1
+        time = 0
         while queue:
-            # print(queue)
-            qlen = len(queue)
-            for _ in range(qlen):
+            # i, j = queue.popleft()
+            qLen = len(queue)
+            for i in range(qLen):
                 i, j = queue.popleft()
+                if mat[i][j] == 1:
+                    mat[i][j] = time
                 for dx, dy in directions:
-                    nx, ny = i + dx, j + dy
-                    # print(visited)
-                    if nx >= 0 and nx < ROWS and ny >= 0 and ny < COLS:
-                        if (nx, ny) not in visited:
-                            if mat[nx][ny] == 1:
-                                mat[nx][ny] = time
-                                queue.append((nx, ny))
-                                visited.add((nx, ny))
+                    nx = i + dx
+                    ny = j + dy
+                    if nx >= 0 and nx < ROWS and ny >= 0 and ny < COLS and (nx, ny) not in visited:
+                        # if mat[nx][ny] == 1:
+                        queue.append((nx, ny))
+                        visited.add((nx, ny))
             time += 1
         return mat
