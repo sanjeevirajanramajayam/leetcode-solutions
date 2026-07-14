ce25dcd66a684d02d10ca4b5913faf170b2b4d3e
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-07-14 09:21:20 +0530

    Time: 171 ms (53.51%) | Memory: 20.7 MB (40.65%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -1,36 +1,22 @@
-class Solution(object):
-    def shortestPathBinaryMatrix(self, grid):
-        """
-        :type grid: List[List[int]]
-        :rtype: int
-        """
-        if grid[0][0] == 1:
-            return -1
-
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
         ROWS = len(grid)
         COLS = len(grid[0])
-        
-        pq = [(1, (0, 0))]
-        directions = [(0, 1), (1, 0), (0, -1), (-1, 0), (1, 1), (-1, 1), (-1, -1), (1, -1)]
-
-        distMat = [[ float('inf') for j in range(COLS)] for i in range(ROWS)]
-        distMat[0][0] = 1
-        # print(distMat)
-
-        while pq:
-            dist, (row, col) = heapq.heappop(pq)
-            if dist > distMat[row][col]:
-                continue
-            for dr, dc in directions:
-                nRow, nCol = row + dr, col + dc
-                if nRow >= 0 and nRow < ROWS and nCol >= 0 and nCol < COLS:
-                    if grid[nRow][nCol] == 0:
-                        # print(nRow, nCol, row, col)
-                        if dist + 1 < distMat[nRow][nCol]:
-                            distMat[nRow][nCol] = dist + 1
-                            heapq.heappush(pq, (dist + 1, (nRow, nCol)))
-        
-        if distMat[ROWS - 1][COLS - 1] == float('inf'):
+        directions = [(1, 0), (0, 1), (1, 1), (-1, 1), (1, -1), (-1, -1), (-1, 0), (0, -1)]
+        if (grid[0][0] == 1):
             return -1
-        
-        return (distMat[ROWS - 1][COLS - 1])
+        queue = deque([(0, 0, 1)])
+        visited = set([(0, 0)])
+        while queue:
+            # print(queue)
+            x, y, d = queue.popleft()
+            if x == ROWS - 1 and y == COLS - 1:
+                return d
+            for dx, dy in directions:
+                nx = x + dx
+                ny = y + dy
+                if nx >= 0 and nx < ROWS and ny >= 0 and ny < COLS and (nx, ny) not in visited:
+                    if grid[nx][ny] == 0:
+                        queue.append((nx, ny, d + 1))
+                        visited.add((nx, ny))
+        return -1
