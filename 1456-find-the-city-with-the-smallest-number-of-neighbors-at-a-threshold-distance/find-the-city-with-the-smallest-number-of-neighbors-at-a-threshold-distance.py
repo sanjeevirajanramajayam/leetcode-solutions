c022fbedc0d473aa8c0c5e86684ddb04bdee14b6
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-07-14 18:40:26 +0530

    Time: 271 ms (39.82%) | Memory: 20.2 MB (83.34%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
@@ -1,35 +1,28 @@
-class Solution(object):
-    def findTheCity(self, n, edges, distanceThreshold):
-        """
-        :type n: int
-        :type edges: List[List[int]]
-        :type distanceThreshold: int
-        :rtype: int
-        """
-        adjMatrix = [[float('inf') for i in range(n)] for i in range(n)]
+class Solution:
+    def findTheCity(self, n: int, edges: List[List[int]], dt: int) -> int:
+        adjMat = [[float('inf') for i in range(n)] for i in range(n)]
 
         for i in range(n):
-            adjMatrix[i][i] = 0
+            adjMat[i][i] = 0
 
-        for i in range(len(edges)):
-            start, end, wt = edges[i]
-            adjMatrix[start][end] = wt
-            adjMatrix[end][start] = wt
+        for startNode, endNode, wt in edges:
+            adjMat[startNode][endNode] = wt
+            adjMat[endNode][startNode] = wt
         
+        # print(adjMat)
         for k in range(n):
             for i in range(n):
                 for j in range(n):
-                    adjMatrix[i][j] = min(adjMatrix[i][j], adjMatrix[i][k] + adjMatrix[k][j])
-        
-        min_val = float('inf')
-        min_node = -1
-
+                    adjMat[i][j] = min(adjMat[i][j], adjMat[i][k] + adjMat[k][j])
+        # print(adjMat)
+        ans = -1
+        mini = float('inf')
         for i in range(n):
-            count = 0
+            cnt = 0
             for j in range(n):
-                if adjMatrix[i][j] <= distanceThreshold:
-                    count += 1
-            if count <= min_val:
-                min_val = count
-                min_node = i
-        return min_node
+                if adjMat[i][j] <= dt:
+                    cnt += 1
+            if cnt <= mini:
+                ans = i
+                mini = cnt
+        return ans
