a36da0cd68709a1c0f29a70180440a74127c3c01
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-07-16 14:41:54 +0530

    Time: 0 ms (100.00%) | Memory: 20.9 MB (10.98%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.py
+++ 801-is-graph-bipartite/is-graph-bipartite.py
@@ -1,18 +1,19 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
-        visited = [-1 for i in range(len(graph))]
+        visited = [-1] * len(graph)
+
         def dfs(i, color):
             visited[i] = color
-            for endNode in graph[i]:
-                if visited[endNode] == color:
+            for newN in graph[i]:
+                if visited[newN] != -1 and visited[newN] == color:
                     return False
-                if visited[endNode] == -1:
-                    if not dfs(endNode, not color):
+                if visited[newN] == -1:
+                    if not dfs(newN, not color):
                         return False
             return True
+        # print(visited)
         for i in range(len(graph)):
             if visited[i] == -1:
                 if not dfs(i, 0):
                     return False
         return True
-
