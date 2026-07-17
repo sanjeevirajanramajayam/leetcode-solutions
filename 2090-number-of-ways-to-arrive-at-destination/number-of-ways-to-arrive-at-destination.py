edfb71058ca90be68502808848ce160126a8e504
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-07-17 07:34:15 +0530

    Time: 11 ms (89.77%) | Memory: 25.3 MB (52.97%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.py
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.py
@@ -1,24 +1,25 @@
 class Solution:
     def countPaths(self, n: int, roads: List[List[int]]) -> int:
-        pq = [(0, 0)]
         ways = [0] * n
         ways[0] = 1
+        # queue = deque([])
+        pq = [(0, 0)]
+        adjList = [[] for i in range(n)]
+        for s, e, dt in roads:
+            adjList[s].append((e, dt))
+            adjList[e].append((s, dt))
         dist = [float('inf')] * n
         dist[0] = 0
-        adjList = [[] for i in range(n)]
-        for start, end, w in roads:
-            adjList[start].append((end, w))
-            adjList[end].append((start, w))
-        
         while pq:
-            dt, node = heapq.heappop(pq)
-            # print(dt, node)
-            for newNode, wt in adjList[node]:
-                if dt + wt < dist[newNode]:
-                    dist[newNode] = dt + wt
-                    ways[newNode] = ways[node]
-                    heapq.heappush(pq, (dist[newNode], newNode))
-                elif dt + wt == dist[newNode]:
-                    ways[newNode] += ways[node]
-        # print(ways)
-        return ways[-1] % (10 ** 9 + 7)
+            time, u = heapq.heappop(pq)
+            if time > dist[u]:
+                continue
+            for nnode, dt in adjList[u]:
+                if dist[u] + dt < dist[nnode]:
+                    dist[nnode] = dist[u] + dt
+                    heapq.heappush(pq, (dist[nnode], nnode))
+                    ways[nnode] = ways[u]
+                elif dist[u] + dt == dist[nnode]:
+                    ways[nnode] += ways[u]
+        # print(ways, dist)
+        return ways[n - 1] % (10 ** 9 + 7)
