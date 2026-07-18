889588ed334669235d612df568b375ed5b567c09
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-07-18 09:24:11 +0530

    Time: 271 ms (51.67%) | Memory: 20.9 MB (59.45%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.py
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.py
@@ -1,23 +1,22 @@
 class Solution:
-    def minimumEffortPath(self, h: List[List[int]]) -> int:
-        ROWS = len(h) 
-        COLS = len(h[0])
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        ROWS = len(heights)
+        COLS = len(heights[0])
         pq = [(0, 0, 0)]
-        visited = set([(0, 0)])
-        distArray = [[float('inf') for i in range(COLS)] for i in range(ROWS)]
-        # print(distArray)
-        distArray[0][0] = 0
-        directions = [(1, 0), ( 0, 1), (-1, 0), (0, -1)]
+        dist = [[float('inf') for i in range(COLS)] for i in range(ROWS)]
+        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        dist[0][0] = 0
         while pq:
-            maxDiff, i, j  = heapq.heappop(pq)
-            if maxDiff > distArray[i][j]:
+            # print(pq)
+            dt, i, j = heapq.heappop(pq)
+            if dt > dist[i][j]:
                 continue
             for dx, dy in directions:
                 nx = i + dx
                 ny = j + dy
                 if nx >= 0 and nx < ROWS and ny >= 0 and ny < COLS:
-                    diff = max(distArray[i][j], abs(h[nx][ny] - h[i][j]))
-                    if diff < distArray[nx][ny]:
-                        distArray[nx][ny] = diff
-                        heapq.heappush(pq, (diff, nx, ny))
-        return distArray[ROWS - 1][COLS - 1]
+                    fh = max(dist[i][j], abs(heights[i][j] - heights[nx][ny]))
+                    if fh < dist[nx][ny]:
+                        dist[nx][ny] = fh
+                        heapq.heappush(pq, (fh, nx, ny))
+        return dist[ROWS - 1][COLS - 1]
