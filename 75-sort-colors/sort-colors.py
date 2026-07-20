bdfedaca66d0a15c4c3c8aef523c694ffc2cbb1b
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-07-20 10:56:44 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (90.09%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,9 +3,12 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        mid = 0
+        # 0000 1111 unsorted 2222
+
         low = 0
-        high = len(arr) - 1
+        high = len(arr) -1
+        mid = 0
+
         while mid <= high:
             if arr[mid] == 1:
                 mid += 1
@@ -13,11 +16,13 @@ class Solution:
                 temp = arr[mid]
                 arr[mid] = arr[low]
                 arr[low] = temp
-                low += 1
                 mid += 1
-            elif arr[mid] == 2:
-                temp = arr[mid] 
-                arr[mid] = arr[high]
-                arr[high] = temp
+                low += 1
+            else:
+                temp = arr[high]
+                arr[high] = arr[mid]
+                arr[mid] = temp
                 high -= 1
         
+
+                
