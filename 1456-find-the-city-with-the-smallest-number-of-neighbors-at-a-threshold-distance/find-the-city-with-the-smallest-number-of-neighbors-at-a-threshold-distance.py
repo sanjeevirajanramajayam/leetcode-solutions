a3c3005e30f278517d80b3dfa9df2371400d3b87
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-07-20 07:21:40 +0530

    Time: 219 ms (61.49%) | Memory: 20.2 MB (82.70%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
@@ -1,28 +1,26 @@
 class Solution:
-    def findTheCity(self, n: int, edges: List[List[int]], d: int) -> int:
+    def findTheCity(self, n: int, edges: List[List[int]], distanceThreshold: int) -> int:
         adjMat = [[float('inf') for i in range(n)] for i in range(n)]
         for i in range(n):
             adjMat[i][i] = 0
-        for startNode, endNode, wt in edges:
-            adjMat[startNode][endNode] = wt
-            adjMat[endNode][startNode] = wt
-        
+        for s, e, d in edges:
+            adjMat[s][e] = (d)
+            adjMat[e][s] = (d)
+
         for k in range(n):
             for i in range(n):
                 for j in range(n):
                     adjMat[i][j] = min(adjMat[i][j], adjMat[i][k] + adjMat[k][j])
+        
         # print(adjMat)
-        maxAns = float('inf')
-        ans = -1
-        for i in range(n):
+        ans = 0
+        maxC = float('inf')
+        for i in adjMat:
             cnt = 0
-            for j in range(n):
-                if i != j:
-                    if adjMat[i][j] <= d:
-                        cnt += 1
-            # print(i, cnt)
-            if cnt <= maxAns:
-                maxAns = cnt
-                ans = i
+            for j in i:
+                if j <= distanceThreshold:
+                    cnt += 1
+            if cnt <= maxC:
+                maxC = cnt
+                ans = adjMat.index(i)
         return ans
-            
