040eabd9c76045ce39f9376fb16019caba999275
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-07-20 09:29:35 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (54.55%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -2,21 +2,23 @@ class Solution:
     def findOrder(self, n: int, p: List[List[int]]) -> List[int]:
         adjList = [[] for i in range(n)]
         inorder = [0] * n
-        queue = deque([])
-        for sn, en in p:
-            adjList[en].append(sn)
-            inorder[sn] += 1
-        for i in range(len(adjList)):
+        for s, e in p:
+            adjList[e].append(s)
+            inorder[s] += 1
+
+        queue = deque()
+        
+        for i in range(len(inorder)):
             if inorder[i] == 0:
                 queue.append(i)
         ans = []
         while queue:
             node = queue.popleft()
             ans.append(node)
-            for nn in adjList[node]:
-                inorder[nn] -= 1
-                if inorder[nn] == 0:
-                    queue.append(nn)
+            for i in adjList[node]:
+                inorder[i] -= 1
+                if inorder[i] == 0:
+                    queue.append(i)
         if len(ans) != n:
             return []
         return ans
