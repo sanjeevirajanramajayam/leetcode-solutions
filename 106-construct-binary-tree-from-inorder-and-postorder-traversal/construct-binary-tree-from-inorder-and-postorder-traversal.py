7758350f4ddffeb2dedbc12ff5d0378ce8f7fd11
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-07-20 10:09:59 +0530

    Time: 3 ms (72.70%) | Memory: 21.3 MB (41.74%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.py
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.py
@@ -1,32 +1,25 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def buildTree(self, inorder, postorder):
-        """
-        :type inorder: List[int]
-        :type postorder: List[int]
-        :rtype: Optional[TreeNode]
-        """
+class Solution:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+        hash = {}
         
-        hashmap = {}
         for i in range(len(inorder)):
-            hashmap[inorder[i]] = i  
+            hash[inorder[i]] = i
         
-        def fn(ins, ends, ps, pe):
-            if (ins > ends or ps > pe):
+        def build(inStart, inEnd, postStart, postEnd):
+            if (inStart > inEnd or postStart > postEnd):
                 return
             
-            root = TreeNode(postorder[pe])
-            inIdx = hashmap[postorder[pe]]
-            rightSize = ends - inIdx
-            root.left = fn(ins, inIdx - 1, ps, pe - rightSize - 1)
-            root.right = fn(inIdx + 1, ends, pe - rightSize, pe - 1)
+            inIdx = hash[postorder[postEnd]]
+            rightSize = inEnd - inIdx
+            root = TreeNode(postorder[postEnd])
+            root.left = build(inStart, inIdx - 1, postStart, postEnd - rightSize - 1)
+            root.right = build(inIdx + 1, inEnd, postStart - rightSize, postEnd - 1)
             return root
-
-        return fn(0, len(inorder) - 1, 0, len(inorder) - 1)
-
-
+        n = len(inorder)
+        return build(0, n - 1, 0, n - 1)
