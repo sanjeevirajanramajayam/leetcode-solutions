a1a193abd08567273eda1b0d7c1411df57361e67
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-07-28 09:03:50 +0530

    Time: 41 ms (70.12%) | Memory: 24.2 MB (31.37%) - LeetSync

--- 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.py
+++ 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.py
@@ -1,21 +1,23 @@
 class Solution:
     def maxSubarraySumCircular(self, nums: List[int]) -> int:
+        currSum1 = 0
+        currSum2 = 0
         maxSum = float('-inf')
         minSum = float('inf')
-        minCurrSum = 0
-        maxCurrSum = 0
 
-        for i in nums:
-            minCurrSum += i
-            maxCurrSum += i
-            maxSum = max(maxSum, maxCurrSum)
-            minSum = min(minSum, minCurrSum)
-            if maxCurrSum < 0:
-                maxCurrSum = 0
-            if minCurrSum > 0:
-                minCurrSum = 0
-        # print(maxSum, minSum)
-        if (sum(nums) - minSum) == 0:
-            return maxSum
-        return max(maxSum, (sum(nums) - minSum))
+        for i in range(len(nums)):
+            currSum1 += nums[i]
+            currSum2 += nums[i]
+
+            maxSum = max(maxSum, currSum1)
+            minSum = min(currSum2, minSum)
+
+            if currSum1 < 0:
+                currSum1 = 0
             
+            if currSum2 > 0:
+                currSum2 = 0
+        # print(maxSum , minSum)
+        if minSum == sum(nums):
+            return maxSum
+        return max(maxSum, sum(nums) - minSum)
