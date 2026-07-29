f67c2b1f82b0a687d72cba1cb56d2e06d2f97ff5
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-07-29 09:39:37 +0530

    Time: 54 ms (43.81%) | Memory: 20.1 MB (15.57%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -1,24 +1,20 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        hash = {}
-        k = len(p)
-        p = Counter(p)
-        if len(s) < k:
+        dic = {}
+        lenP = len(p)
+        if len(p) > len(s):
             return []
-        for i in range(k):
-            hash[s[i]] = hash.get(s[i], 0) + 1
+        for i in range(0, len(p)):
+            dic[s[i]] = dic.get(s[i], 0) + 1
+        p = Counter(p)
         ans = []
-        if hash == p:
+        if dic == p:
             ans.append(0)
-        # print(hash)
-        for i in range(k, len(s)):
-            # if s[i - k] in 'aeiou':
-            hash[s[i - k]] = hash.get(s[i - k], 0) - 1
-            if hash[s[i - k]] == 0:
-                del hash[s[i - k]]
-            # if s[i] in 'aeiou':
-            hash[s[i]] = hash.get(s[i], 0) + 1
-            # print(hash)
-            if hash  == p :
-                ans.append(i - k + 1)     
+        for i in range(lenP, len(s)):
+            dic[s[i - lenP]] -= 1
+            if dic[s[i - lenP]] == 0:
+                del dic[s[i - lenP]] 
+            dic[s[i]] = dic.get(s[i], 0) + 1
+            if dic == p:
+                ans.append(i - lenP + 1)
         return ans
