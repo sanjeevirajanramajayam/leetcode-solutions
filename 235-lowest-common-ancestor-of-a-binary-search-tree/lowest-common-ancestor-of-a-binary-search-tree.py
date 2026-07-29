973dd7e3aef06b4567c61e41b83efbbe615b623a
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-07-29 20:34:28 +0530

    Time: 71 ms (22.54%) | Memory: 22.7 MB (67.55%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,37 +7,17 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        def lca(root):
-            if root == None:
-                return
-
-                
-            if root.val == p.val:
-                return root
-            
-            if root.val == q.val:
-                return root
-
-
-            left = lca(root.left)
-            right = lca(root.right)
-            # print(root.val)
-            # if left:
-            #     print(left.val)
-            # else:
-            #     print("None")
-            # if right:
-            #     print(right.val)
-            # else:
-            #     print("None")
-
-            if left and right:
-                return root
-
-            if left:
-                return left
-            
-            if right:
-                return right
-        
-        return lca(root)
+        def fn(node):
+            if node == None:
+                return None
+            # print(node.val, p.val, q.val)
+            if node.val > p.val and node.val > q.val:
+                if fn(node.left) is not None:
+                    return fn(node.left) 
+            elif node.val < p.val and node.val < q.val:
+                if fn(node.right) is not None:
+                    return fn(node.right)
+            else:
+                # print(node.val)
+                return node
+        return fn(root)
