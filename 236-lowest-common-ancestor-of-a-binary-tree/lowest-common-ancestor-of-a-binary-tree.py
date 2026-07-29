e0a29793a2fc7ec7d26377e7dcdcdbcdac3c2fd5
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-07-29 10:12:49 +0530

    Time: 139 ms (34.56%) | Memory: 49.4 MB (72.44%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -1,43 +1,32 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
 
-class Solution(object):
-    def lowestCommonAncestor(self, root, p, q):
-        """
-        :type root: TreeNode
-        :type p: TreeNode
-        :type q: TreeNode
-        :rtype: TreeNode
-        """
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         def fn(root):
-            if root == None:
+            if (root == None):
                 return None
-
+            lh = fn(root.left)
+            rh = fn(root.right)
+            # print(lh, rh, root.val)
             if root.val == p.val:
                 return root
             
             if root.val == q.val:
                 return root
 
-            left = fn(root.left)
-            right = fn(root.right)
-
-            if left != None and right != None:
+            if lh and rh:
                 return root
 
-            if left != None:
-                return left
-            
-            if right != None:
-                return right
+            if lh and not rh:
+                return lh
+
+            if rh and not lh:
+                return rh
+
+
         return fn(root)
-
-            
-
-
-
-        
