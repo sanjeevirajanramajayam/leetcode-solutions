ad2adb214c33a256c3bb15cf3faa19447bcae3df
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-07-30 18:43:14 +0530

    Time: 119 ms (25.26%) | Memory: 23.6 MB (34.86%) - LeetSync

--- 2755-extra-characters-in-a-string/extra-characters-in-a-string.py
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.py
@@ -1,17 +1,15 @@
 class Solution:
     def minExtraChar(self, s: str, dictionary: List[str]) -> int:
         dictionary = set(dictionary)
-        n = len(s)
         @cache
         def fn(ind):
-            if ind == n:
+            mini = float('inf')
+            if ind == len(s):
                 return 0
-            
-            mini = 1 + fn(ind + 1)
-            for i in range(ind, n):
-                if s[ind:i + 1] in dictionary:
-                    mini = min( mini , fn(i + 1))
-
+            skipped = 1 + fn(ind + 1)
+            mini = min(mini, skipped)
+            for i in range(ind, len(s)):
+                if s[ind:i + 1] in dictionary: 
+                    mini = min(mini, fn(i + 1))
             return mini
-
         return fn(0)
