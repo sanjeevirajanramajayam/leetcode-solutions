f71ea51073489f782d196c28d1dc15f89938bc00
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-08-03 18:51:46 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (19.98%) - LeetSync

--- 1304-longest-happy-string/longest-happy-string.py
+++ 1304-longest-happy-string/longest-happy-string.py
@@ -1,32 +1,23 @@
 class Solution:
     def longestDiverseString(self, a: int, b: int, c: int) -> str:
-        heap = [(-a, 'a'), (-b, 'b'), (-c, 'c')]
+        heap = [(-c, 'c', 0), (-b, 'b', 0), (-a, 'a', 0)]
         heapq.heapify(heap)
-        last_char = (-1, 0)
-        s = ""
+        ans = ""
         temp = None
+        
         while heap:
-            freq, char = heapq.heappop(heap)
-            if freq >= 0:
+            # print(heap, ans, temp)
+            freq, char, time = heapq.heappop(heap)
+            if freq == 0:
                 continue
-            s += char
-            # print(s, freq, char, heap, temp, last_char)
-
+            ans += char
             if temp:
-                if temp[0] < 0:
-                    heapq.heappush(heap, temp)
+                heapq.heappush(heap, (temp[0], temp[1], temp[2]))
                 temp = None
-            
-            if last_char[0] != char:
-                last_char = (char, 1)
-            else:
-                last_char = (char, last_char[1] + 1)
-            
-            if last_char[1] == 2:
-                temp = (freq + 1, char)
+            if time == 1:
+                if freq + 1 < 0:
+                    temp = (freq + 1, char, 0)
             else:
                 if freq + 1 < 0:
-                    heapq.heappush(heap, (freq + 1 , char))
-        return s
-            
-            
+                    heapq.heappush(heap, (freq + 1, char, time + 1))
+        return ans
