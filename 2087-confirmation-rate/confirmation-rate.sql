d8ebce169d213d7494af19bd8cbf9fbcdfedb92c
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-08-06 21:24:07 +0530

    Time: 806 ms (23.95%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -1,6 +1,2 @@
 # Write your MySQL query statement below
-select tot.user_id, COALESCE(ROUND(confirm_count / total, 2), 0) as confirmation_rate from (select user_id, count(user_id) as confirm_count from Confirmations where action = 'confirmed' group by user_id) as tou RIGHT JOIN (select su.user_id, count(c.user_id) as total from Confirmations c RIGHT JOIN Signups su on c.user_id = su.user_id group by su.user_id) as tot on tou.user_id = tot.user_id 
-
--- (select su.user_id, count(c.user_id) as total from Confirmations c RIGHT JOIN Signups su on c.user_id = su.user_id group by su.user_id)
-
--- (select su.user_id, count(c.user_id) as confirm_count from Confirmations c RIGHT JOIN Signups su on c.user_id = su.user_id group by su.user_id)
+select s.user_id, COALESCE(ROUND(sum(action = 'confirmed')/count(*), 2), 0)  as confirmation_rate from Signups s left join confirmations c on s.user_id = c.user_id group by s.user_id
