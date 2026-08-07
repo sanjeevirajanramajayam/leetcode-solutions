2112df8c7abc9125df13d6860e98c3a902235bf0
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-08-07 18:39:05 +0530

    Time: 139 ms (48.11%) | Memory: 22 MB (40.33%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.py
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.py
@@ -3,8 +3,8 @@ class Solution:
         def lessThanK(k):
             if k < 0:
                 return 0
-            cnt = 0
             l = 0
+            ans = 0
             hash = {}
             for r in range(len(nums)):
                 hash[nums[r]] = hash.get(nums[r], 0) + 1
@@ -12,7 +12,7 @@ class Solution:
                     hash[nums[l]] = hash.get(nums[l], 0) - 1
                     if hash[nums[l]] == 0:
                         del hash[nums[l]]
-                    l+=1
-                cnt += (r - l) + 1
-            return cnt
+                    l += 1
+                ans += r - l + 1
+            return ans
         return lessThanK(k) - lessThanK(k - 1)
