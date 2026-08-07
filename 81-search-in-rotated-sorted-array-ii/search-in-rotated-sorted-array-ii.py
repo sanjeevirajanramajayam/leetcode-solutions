e32135d6c33137be7f28d03e5d10c2e2f9cde98f
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-08-07 10:51:48 +0530

    Time: 0 ms (100.00%) | Memory: 19.8 MB (9.89%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
@@ -1,27 +1,26 @@
 class Solution:
-    def search(self, arr: List[int], target: int) -> bool:
+    def search(self, nums: List[int], target: int) -> bool:
         low = 0
-        high = len(arr) - 1
-        while low <= high:
-            # print(low, high)
-            mid = (low + high) // 2
+        high = len(nums) - 1
 
-            if arr[mid] == target:
+        while low <= high:
+            mid = (low + high) // 2
+            if nums[mid] == target:
                 return True
-            if arr[mid] == arr[low] and arr[mid] == arr[high]:
+            if nums[mid] == nums[low] == nums[high]:
                 low += 1
                 high -= 1
-            elif arr[low] <= arr[mid]:
-                # print(arr[low], arr[mid])
-                if target >= arr[low] and target < arr[mid]:
+                continue
+
+            if nums[low] <= nums[mid]:
+                # print(target, nums[low], nums[mid])
+                if target >= nums[low] and target < nums[mid]:
                     high = mid - 1
-                    # print(high)
                 else:
                     low = mid + 1
-            elif arr[mid] <= arr[high]:
-                if target > arr[mid] and target <= arr[high]:
+            elif nums[mid] <= nums[high]:
+                if target > nums[mid] and target <= nums[high]:
                     low = mid + 1
                 else:
                     high = mid - 1
-         
         return False
