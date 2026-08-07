c6a70723f43deaf23d883083c959b4e4af408809
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-08-07 07:18:24 +0530

    Time: 155 ms (19.82%) | Memory: 19.8 MB (15.25%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,20 +1,31 @@
-class Solution(object):
-    def characterReplacement(self, s, k):
-        """
-        :type s: str
-        :type k: int
-        :rtype: int
-        """
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        freqMap = {}
         l = 0
-        maxLen = 0
-        maxFreq = 0
-        charHash = {}
+        maxFreq = float('-inf')
+        maxChar = '-1' 
+        ans = 0
         for r in range(len(s)):
-            charHash[s[r]] = charHash.get(s[r], 0) + 1
-            maxFreq = max(charHash[s[r]], maxFreq)
-            while (r - l + 1) - maxFreq > k:
-                charHash[s[l]] -= 1
+            freqMap[s[r]] = freqMap.get(s[r], 0) + 1
+
+            if freqMap[s[r]] > maxFreq:
+                maxChar = s[r]
+                maxFreq = freqMap[s[r]]
+
+            while maxFreq + k < r - l + 1:
+                freqMap[s[l]] -= 1
+                if freqMap[s[l]] == 0:
+                    del freqMap[s[l]]
+
+                maxFreq = float('-inf')
+                maxChar = '-1' 
+
+                for key in freqMap:
+                    if freqMap[key] > maxFreq:
+                        maxChar = key
+                        maxFreq = freqMap[key]
+                
                 l += 1
-            if (r - l + 1) - maxFreq <= k:
-                maxLen = max(maxLen, (r - l + 1))
-        return maxLen
+            
+            ans = max(ans, r - l + 1)
+        return ans
