364e313470f18795a16f65087b6cfd8e816c81b6
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-08-07 07:03:58 +0530

    Time: 39 ms (32.76%) | Memory: 20.2 MB (62.43%) - LeetSync

---
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
@@ -0,0 +1,19 @@
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], k: int) -> int:
+        def lessThanK(k):
+            if k < 0:
+                return 0
+            l = 0
+            oneCnt = 0
+            ans = 0
+            for r in range(len(nums)): 
+                if nums[r] == 1:
+                    oneCnt += 1
+                while oneCnt > k:
+                    if nums[l] == 1:
+                        oneCnt -= 1
+                    l += 1
+                ans += (r - l) + 1
+                # print(ans, r, l)
+            return ans
+        return lessThanK(k) - lessThanK(k - 1)
