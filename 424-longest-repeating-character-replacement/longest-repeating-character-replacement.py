1f42a50d60ad7edb8e39daec028ffc977e08a269
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-08-10 18:56:43 +0530

    Time: 75 ms (88.85%) | Memory: 19.7 MB (51.02%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,31 +1,19 @@
 class Solution:
-    def characterReplacement(self, s: str, k: int) -> int:
-        freqMap = {}
-        l = 0
+    def characterReplacement(self, nums: str, k: int) -> int:
         maxFreq = float('-inf')
-        maxChar = '-1' 
-        ans = 0
-        for r in range(len(s)):
-            freqMap[s[r]] = freqMap.get(s[r], 0) + 1
-
-            if freqMap[s[r]] > maxFreq:
-                maxChar = s[r]
-                maxFreq = freqMap[s[r]]
-
-            while maxFreq + k < r - l + 1:
-                freqMap[s[l]] -= 1
-                if freqMap[s[l]] == 0:
-                    del freqMap[s[l]]
-
-                # maxFreq = float('-inf')
-                # maxChar = '-1' 
-
-                # for key in freqMap:
-                #     if freqMap[key] > maxFreq:
-                #         maxChar = key
-                #         maxFreq = freqMap[key]
-                
+        maxChar = -1
+        freqHash = {}
+        ans = float('-inf')
+        l = 0
+        for r in range(len(nums)):
+            freqHash[nums[r]] = freqHash.get(nums[r], 0) + 1
+            if freqHash[nums[r]] > maxFreq:
+                maxFreq = freqHash[nums[r]]
+                # maxChar = nums[r]
+            # print(freqHash)
+            # print(maxFreq)
+            while maxFreq + k < (r - l + 1):
+                freqHash[nums[l]] -= 1
                 l += 1
-            
             ans = max(ans, r - l + 1)
         return ans
