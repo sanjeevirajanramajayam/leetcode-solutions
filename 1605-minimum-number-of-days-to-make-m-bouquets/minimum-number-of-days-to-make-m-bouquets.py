aef5d12ad6bdc7bb7e577459b858fed74c0b10b7
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-08-12 09:33:02 +0530

    Time: 492 ms (13.62%) | Memory: 32.3 MB (43.54%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.py
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.py
@@ -1,34 +1,26 @@
-class Solution(object):
-    def minDays(self, bloomDay, m, k):
-        """
-        :type bloomDay: List[int]
-        :type m: int
-        :type k: int
-        :rtype: int
-        """
+class Solution:
+    def minDays(self, bloomDay: List[int], m: int, k: int) -> int:
+        low = 1
+        high = max(bloomDay)
+        
         def fn(day):
-            bt = 0
-            cf = 0
+            b = 0
+            f = 0
             for i in range(len(bloomDay)):
                 if bloomDay[i] <= day:
-                    cf += 1
+                    f += 1
+                    if f == k:
+                        b += 1
+                        f = 0
                 else:
-                    cf = 0
-                if cf == k:
-                    bt += 1
-                    cf = 0
-            return bt
-        
-        low = min(bloomDay)
-        high = max(bloomDay)
-        if len(bloomDay) < m * k:
-            return -1
+                    f = 0
+            return b
         ans = -1
         while low <= high:
             mid = (low + high) // 2
-            print(low, high, fn(mid))
+            # print(mid, fn(mid))
             if fn(mid) >= m:
-                ans = mid 
+                ans = mid
                 high = mid - 1
             else:
                 low = mid + 1
