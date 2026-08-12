c83e93b01f9bc6e17036e6ae3adbe13b67a5db53
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-08-12 20:43:34 +0530

    Time: 2247 ms (13.17%) | Memory: 567.6 MB (5.11%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,27 +1,16 @@
 class Solution:
-    def canPartition(self, nums: List[int]) -> bool:
-        def isSubsetSum (arr, target):
-            dp = [[False for i in range(target + 1)] for j in range(len(arr) + 1)]
-            prev = [False for i in range(target + 1)] 
-            dp = [False for i in range(target + 1)] 
-            
-            prev[0] = True
-            dp[0] = True
-            if arr[0] <= target:
-                dp[arr[0]] = True
-                prev[arr[0]] = True
-            
-            for i in range(1, len(arr)):
-                for j in range(1, target + 1):
-                    take = False
-                    if j >= arr[i]:
-                        take = prev[j - arr[i]]
-                    not_take = prev[j]
-                    dp[j] = take or not_take
-                prev = dp[:]
-            # print(dp, prev)
-            return dp[target]
-        arrSum = sum(nums)
-        if arrSum % 2 == 1:
-            return False
-        return isSubsetSum(nums, arrSum // 2)
+    def canPartition(self, arr: List[int]) -> bool:
+        @cache
+        def fn(ind, target):
+            if target == 0:
+                return True
+
+            if ind == 0:
+                return arr[0] == target
+
+            if arr[ind] <= target and fn(ind - 1, target - arr[ind]):
+                return True
+
+            return fn(ind - 1, target)
+
+        return fn(len(arr) - 1, sum(arr) / 2)
