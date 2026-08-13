797e39961dd608c76c55b5c6019998057069ceb1
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-08-13 14:30:25 +0530

    Time: 88 ms (39.35%) | Memory: 21.1 MB (81.68%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.py
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.py
@@ -1,17 +1,11 @@
 class Solution:
-    def minDistance(self, text1: str, text2: str) -> int:
-        dp = [[-1 for i in range(len(text2) + 1 )] for j in range(len(text1) + 1 )]
+    def minDistance(self, s: str, s2: str) -> int:
+        dp = [[0 for _ in range(len(s2) + 1)] for _ in range(len(s) + 1)]
+        for i in range(1, len(s) + 1):
+            for j in range(1, len(s2) + 1):
+                if s[i - 1] == s2[j - 1]:
+                    dp[i][j] = dp[i - 1][ j - 1] + 1
+                else:
+                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j ])
         
-        for i in range(len(text1) + 1):
-            dp[i][0] = 0
-        for i in range((len(text2) + 1)):
-            dp[0][i] = 0
-
-        for i in range(1, len(text1) + 1):
-            for j in range(1, len(text2) + 1):
-                if text1[i - 1] == text2[j - 1]:
-                    dp[i][j] = 1 + dp[i - 1][j - 1]
-                    continue
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
-        
-        return len(text1) + len(text2) - 2 * dp[len(text1)][len(text2)]
+        return len(s) + len(s2) + - 2* dp[len(s)][len(s2)]
