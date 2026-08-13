fbe602b431d3b07c68f565c29549d5c6031e2307
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-08-13 10:59:24 +0530

    Time: 1389 ms (5.23%) | Memory: 482.5 MB (5.01%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,21 +1,24 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [[-1 for j in range(amount + 1)] for i in range(len(coins))]
-        def fn(ind, curr):
+        @cache
+        def fn(ind, target):
+            # print(ind, target)
+            if target == 0:
+                return 0
+            
             if ind == 0:
-                if curr % coins[ind] == 0:
-                    return curr // coins[ind]
+                if target % coins[ind] == 0:
+                    return target // coins[ind]
                 else:
                     return float('inf')
-            if dp[ind][curr] != -1:
-                return dp[ind][curr]
             take = float('inf')
-            if coins[ind] <= curr:
-                take = 1 + fn(ind, curr - coins[ind])
-            not_take = fn(ind - 1, curr)
-            dp[ind][curr] = min(take, not_take)
+            if target >= coins[ind]:
+                take = 1 + fn(ind, target - coins[ind])
+                # print(coins[ind], take - 1, 1)
+            not_take = fn(ind - 1, target)
+
             return min(take, not_take)
-        ans = fn(len(coins) - 1, amount)
-        if ans == float('inf'):
+        x = fn(len(coins) - 1, amount) 
+        if x == float('inf'):
             return -1
-        return ans
+        return x
