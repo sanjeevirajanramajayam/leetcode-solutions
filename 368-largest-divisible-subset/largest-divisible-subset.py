19462729dedec11997262704008d946957766350
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-08-17 09:33:31 +0530

    Time: 172 ms (36.57%) | Memory: 19.5 MB (53.88%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.py
+++ 368-largest-divisible-subset/largest-divisible-subset.py
@@ -1,17 +1,25 @@
 class Solution:
-    def largestDivisibleSubset(self, words: List[int]) -> List[int]:
-        dp = [1 for i in range(len(words))]
-        path = [i for i in range(len(words))]
-        words.sort()
-        for i in range(len(words)):
+    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        dp = [1] * len(nums)
+        back = [i for i in range(len(nums))]
+
+        for i in range(len(nums)):
             for j in range(0, i):
-                if words[i] % words[j] == 0 and dp[j] + 1 > dp[i]:
-                    dp[i] = dp[j] + 1
-                    path[i] = j
-        index = dp.index(max(dp))
+                if nums[i] % nums[j] == 0:
+                    if dp[j] + 1 > dp[i]:
+                        dp[i] = max(dp[i], dp[j] + 1)
+                        back[i] = j  
+        print(dp)
+        print(back)
         ans = []
-        while index != path[index]:
-            ans.append(words[index])
-            index = path[index]
-        ans.append(words[index])
-        return ans[::-1]
+        req_ind = dp.index(max(dp))
+        # print(req_ind)
+        ans.append(nums[req_ind])
+        next = back[req_ind]
+        while req_ind != next:
+            # print(req_ind, next)
+            ans.append(nums[next])
+            req_ind = back[req_ind]
+            next = back[req_ind]
+        return ans
