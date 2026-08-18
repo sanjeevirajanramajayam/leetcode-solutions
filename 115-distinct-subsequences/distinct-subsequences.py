98fc81e986ca557d79f8cfd1b16ec2b49bffa6f4
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-08-18 07:22:04 +0530

    Time: 631 ms (22.79%) | Memory: 218.4 MB (20.24%) - LeetSync

---
+++ 115-distinct-subsequences/distinct-subsequences.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        @cache
+        def fn(i, j):
+            if j < 0:
+                return 1
+            if i < 0:
+                return 0
+            
+            if s[i] == t[j]:
+                return fn(i - 1, j - 1) + fn(i - 1, j)
+            else:
+                return fn(i - 1, j)
+            
+        return fn(len(s) - 1, len(t) - 1)
