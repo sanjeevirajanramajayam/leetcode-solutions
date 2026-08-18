9abf389a63eea4a5050f0cf6a040b089f172cd8b
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-08-18 15:12:03 +0530

    Time: 2695 ms (12.49%) | Memory: 22.5 MB (73.89%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.py
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.py
@@ -1,19 +1,15 @@
 class Solution:
     def minCut(self, s: str) -> int:
-        if s == s[::-1]:
-            return 0
-        dp = {}
+        @cache
         def fn(i):
             if i == len(s):
                 return 0
-            if i in dp:
-                return dp[i]
+            ans = float('inf')
             temp = ""
-            mini = float('inf')
-            for j in range(i, len(s)):
+
+            for j in range(i,  len(s)):
                 temp += s[j]
                 if temp == temp[::-1]:
-                    mini = min(mini, 1 + fn(j + 1))
-            dp[i] = mini
-            return mini
+                    ans = min(ans, 1 + fn(j + 1))
+            return ans 
         return fn(0) - 1
