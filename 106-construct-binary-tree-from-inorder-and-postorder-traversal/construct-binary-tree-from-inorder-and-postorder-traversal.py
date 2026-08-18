ea2467418e9937cedbf9d4c7bf60373c0aebcfc0
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-08-18 09:40:54 +0530

    Time: 3 ms (73.13%) | Memory: 21.2 MB (48.43%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.py
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.py
@@ -7,19 +7,20 @@
 class Solution:
     def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
         hash = {}
-        
+
         for i in range(len(inorder)):
             hash[inorder[i]] = i
-        
-        def build(inStart, inEnd, postStart, postEnd):
+
+        print(hash)
+
+        def fn(inStart, inEnd, postStart, postEnd):
             if (inStart > inEnd or postStart > postEnd):
-                return
+                return None
             
+            root = TreeNode(postorder[postEnd])
             inIdx = hash[postorder[postEnd]]
             rightSize = inEnd - inIdx
-            root = TreeNode(postorder[postEnd])
-            root.left = build(inStart, inIdx - 1, postStart, postEnd - rightSize - 1)
-            root.right = build(inIdx + 1, inEnd, postStart - rightSize, postEnd - 1)
+            root.left = fn(inStart, inIdx - 1, postStart, postEnd - rightSize - 1)
+            root.right = fn(inIdx + 1, inEnd, postEnd - rightSize, postEnd - 1)
             return root
-        n = len(inorder)
-        return build(0, n - 1, 0, n - 1)
+        return fn(0, len(inorder) - 1, 0, len(inorder) - 1)
