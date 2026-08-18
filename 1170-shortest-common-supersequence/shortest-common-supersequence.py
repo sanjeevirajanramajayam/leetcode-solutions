e7d1aef01f0735d65b6ae34aa29967f8f8708476
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-08-18 07:07:54 +0530

    Time: 462 ms (18.68%) | Memory: 45.7 MB (20.51%) - LeetSync

---
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.py
@@ -0,0 +1,37 @@
+class Solution:
+    def shortestCommonSupersequence(self, s1: str, s2: str) -> str:
+        dp = [[0 for i in range(len(s2) + 1)] for i in range(len(s1) + 1)]
+        
+        for i in range(1, len(s1) + 1):
+            for j in range(1, len(s2) + 1):
+                if s1[i - 1] == s2[j - 1]:
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        
+        i = len(s1)
+        j = len(s2)
+        ans = ""
+        for x in dp:
+            print(x)
+        while i > 0 and j > 0:
+            if s1[i - 1] == s2[j - 1]:
+                ans += s1[i - 1]
+                i -= 1
+                j -= 1
+            elif dp[i - 1][j] > dp[i][j - 1]:
+                ans += s1[i - 1]
+                i -= 1
+            else:
+                ans += s2[j - 1]
+                j -= 1
+        # print(i, j)
+        while i > 0:
+            ans += s1[i - 1]
+            i -= 1
+         
+        while j > 0:
+            ans += s2[j - 1]
+            j -= 1
+        
+        return ans[::-1]
