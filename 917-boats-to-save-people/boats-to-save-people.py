16f792225e86144870440d2e13285d4f402cd410
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-08-18 11:30:45 +0530

    Time: 50 ms (68.57%) | Memory: 25.1 MB (53.29%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.py
+++ 917-boats-to-save-people/boats-to-save-people.py
@@ -3,14 +3,13 @@ class Solution:
         people.sort()
         l = 0
         r = len(people) - 1
-        cnt = 0
-        # print(people)
+        boats = 0
         while l <= r:
-            if people[l] + people[r] <= limit:
-                cnt += 1
+            if people[r] + people[l] <= limit:
+                r -= 1
                 l += 1
-                r -= 1
+                boats += 1
             else:
-                cnt += 1
                 r -= 1
-        return cnt
+                boats += 1
+        return boats
