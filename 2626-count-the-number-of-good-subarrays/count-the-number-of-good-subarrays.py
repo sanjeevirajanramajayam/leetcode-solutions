1c9c5596c6ed9a98c7a2c3d9a3ac3150a3ad37f9
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-08-19 06:41:21 +0530

    Time: 127 ms (54.40%) | Memory: 36.7 MB (79.87%) - LeetSync

--- 2626-count-the-number-of-good-subarrays/count-the-number-of-good-subarrays.py
+++ 2626-count-the-number-of-good-subarrays/count-the-number-of-good-subarrays.py
@@ -1,18 +1,17 @@
 class Solution:
     def countGood(self, nums: List[int], k: int) -> int:
         l = 0
-        hash = {}
         pairs = 0
-        ans = l
+        hash = {}
+        ans = 0
         for r in range(len(nums)):
             hash[nums[r]] = hash.get(nums[r], 0) + 1
             pairs += (hash[nums[r]] - 1)
-            # print(pairs, r, nums[r], hash)
+            # print(nums[l:r+1], pairs, hash)
             while pairs >= k:
-                # print(pairs)
                 hash[nums[l]] = hash.get(nums[l], 0) - 1
                 pairs -= hash[nums[l]]
                 l += 1
-            # print(l)
+            
             ans += l
         return ans
