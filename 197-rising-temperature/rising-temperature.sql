52c7f5101910f917f7d1f6ae35268a5a25527dee
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-08-19 17:05:06 +0530

    Time: 578 ms (24.98%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,2 +1,7 @@
 # Write your MySQL query statement below
-select w1.id from Weather w1 join Weather w2 on DATEDIFF(w1.recordDate, w2.recordDate) = 1 where w2.temperature < w1.temperature;
+-- select id from (
+-- select id, temperature , lag(temperature ) over(order by recordDate asc) as temp from weather 
+-- ) as b where b.temp is not null and b.temp < b.temperature 
+
+select a.id from Weather  a join Weather  b on datediff(a.recordDate, b.recordDate) = 1
+where a.temperature > b.temperature
