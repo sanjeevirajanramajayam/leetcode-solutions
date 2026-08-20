10c2e9a3db80c5a869ebd200663372996f34e3d4
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-08-20 09:48:20 +0530

    Time: 51 ms (93.85%) | Memory: 19.8 MB (11.52%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,28 +1,30 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        hash = {}
+        freqMap = {}
+        
         for i in t:
-            hash[i] = hash.get(i, 0) + 1
-        oldLen = len(hash)
-        startChar = -1
-        ans = float('inf')
-        cnt = 0
+            freqMap[i] = freqMap.get(i, 0) + 1
+        prevLen = len(freqMap)
         l = 0
+        cnt = 0
+        start = -1
+        alen = float('inf')
         for r in range(len(s)):
-            hash[s[r]] = hash.get(s[r], 0) - 1
-            if hash[s[r]] == 0:
+            freqMap[s[r]] = freqMap.get(s[r], 0) - 1
+            if freqMap[s[r]] == 0:
                 cnt += 1
-            # print(hash, cnt)
-            while cnt == oldLen:
-                if ans > r - l + 1:
-                    ans = r- l + 1
-                    startChar = l
-                hash[s[l]] = hash.get(s[l], 0) + 1
-                if hash[s[l]] > 0:
+            # print(cnt, len(freqMap))
+            while cnt >= prevLen:
+                # print(s[l:r+1])
+                if cnt == prevLen:
+                    if alen > (r - l + 1):
+                        alen = (r - l + 1)        
+                        start = l
+                freqMap[s[l]] += 1
+                if freqMap[s[l]] > 0:
                     cnt -= 1
                 l += 1
-                # print(cnt, l, s[l:r+1])
-        if startChar == -1:
+        # print(start, start + alen)
+        if start == -1:
             return ""
-        return s[startChar:startChar+ans]
-        print(startChar, ans)
+        return s[start:start+alen]
