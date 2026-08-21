64af3382081ece535a79559fc70a2bdaedf88496
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-08-21 14:35:55 +0530

    Time: 2469 ms (7.33%) | Memory: 20.5 MB (9.92%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -1,19 +1,24 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        lis = []
-        for i in range(len(nums)):
-            if not lis or nums[i] > lis[-1]:
-                lis.append(nums[i])
-            else:
-                l = 0
-                r = len(lis) - 1
-                ans = -1
-                while l <= r:
-                    mid = (l + r) // 2
-                    if lis[mid] >= nums[i]:
-                        ans = mid
-                        r = mid - 1
-                    else:
-                        l = mid + 1
-                lis[ans] = nums[i]
-        return len(lis)
+        # maxi = float('-inf')
+        @cache
+        def fn(idx, last):
+            if idx == len(nums):
+                return 0
+            maxi = float('-inf')
+            if last == -1 or nums[idx] > nums[last]:
+                maxi = max(maxi, 1 + fn(idx + 1, idx))
+            
+            maxi = max(maxi, fn(idx + 1, last))
+
+            return maxi
+        # return fn(0, -1)
+        dp = [1 for i in range(len(nums))]
+
+        for i in range(len(nums) + 1):
+            for j in range(1, i):
+                # print(i - 1, j - 1, nums[j - 1], nums[i - 1])
+                if nums[j - 1] < nums[i - 1]:
+                    dp[i - 1] = max(dp[i - 1], dp[j - 1] + 1)
+        # print(dp)
+        return max(dp)
