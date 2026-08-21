270bfd704b281f4aa83e07c4ff0d839cde1ef5ba
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-08-21 13:00:08 +0530

    Time: 570 ms (54.53%) | Memory: 19.6 MB (63.24%) - LeetSync

--- 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.py
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.py
@@ -1,14 +1,13 @@
 class Solution:
     def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
-        arr.sort()
         heap = []
-        for i in range(len(arr)):
-            heapq.heappush(heap, (arr[i] / arr[len(arr) - 1], i, len(arr) - 1, arr[i], arr[len(arr) - 1]))
+        for i in range(min(len(arr) - 1, k)):
+            heapq.heappush(heap, (arr[i] / arr[-1], i, len(arr) - 1))
+        
         while k > 0:
-            # print(heap)
-            fraction, numerator, denominator, _, _ = heapq.heappop(heap)
-            if denominator - 1 >= 0:
-                heapq.heappush(heap, (arr[numerator]/ arr[denominator - 1], numerator, denominator - 1, arr[numerator], arr[denominator - 1]))
+            fraction, numerator, denominator = heapq.heappop(heap)
+            if denominator - 1 > numerator:
+                heapq.heappush(heap, (arr[numerator] / arr[denominator - 1], numerator, denominator - 1))
             k -= 1
             # print(heap)
         return [arr[numerator], arr[denominator]]
