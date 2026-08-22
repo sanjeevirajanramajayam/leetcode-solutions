2bcc7b1f0f972e7d0ffeb1bb4244efb6b9c335f7
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-08-22 16:29:53 +0530

    Time: 811 ms (16.09%) | Memory: 37.6 MB (12.49%) - LeetSync

--- 948-sort-an-array/sort-an-array.py
+++ 948-sort-an-array/sort-an-array.py
@@ -1,33 +1,36 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        def merge(low, mid, high, nums):
-            left = low
-            right = mid + 1
-            temp = []
-            while left <= mid and right <= high:
-                if nums[left] <= nums[right]:
-                    temp.append(nums[left])
-                    left += 1
+        def merge(l1, r1, l2, r2):
+            # print(l1, r1, l2, r2)
+            newArr = []
+            i = l1
+            j = l2
+            while i <= r1 and j <= r2:
+                if nums[i] > nums[j]:
+                    newArr.append(nums[j])
+                    j += 1
                 else:
-                    temp.append(nums[right])
-                    right += 1
-            while left <= mid:
-                temp.append(nums[left])
-                left += 1
-            while right <= high:
-                temp.append(nums[right])
-                right += 1
-            # print(low, mid, high)
-            for i in range(low, high + 1):
-                nums[i] = temp[i-low]
+                    newArr.append(nums[i])
+                    i += 1
+            while i <= r1:
+                newArr.append(nums[i])
+                i += 1     
+            while j <= r2:
+                newArr.append(nums[j])
+                j += 1
+            # print(newArr)
+            for i in range(len(newArr)):
+                nums[i + l1] = newArr[i]
+            # print(nums)
 
-        def mergeSort(low, high, nums):
-            if low >= high:
+        def mergeSort(left, right):
+            # print
+            # print(left, right)
+            if left >= right or right < left:
                 return
-            mid = (low + high) // 2
-            mergeSort(low, mid, nums)
-            mergeSort(mid + 1, high, nums)
-            merge(low, mid, high, nums)
-        
-        mergeSort(0, len(nums) - 1, nums)
+            mid = (left + right) // 2
+            mergeSort(left, mid)
+            mergeSort(mid + 1, right)
+            merge(left, mid, mid+1, right)
+        mergeSort(0, len(nums) - 1)
         return nums
