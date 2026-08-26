450d7608a8ac55071b446c84b66401a2d681da4f
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-08-26 22:17:36 +0530

    Time: 1006 ms (49.23%) | Memory: 40 MB (35.01%) - LeetSync

--- 854-making-a-large-island/making-a-large-island.py
+++ 854-making-a-large-island/making-a-large-island.py
@@ -1,69 +1,70 @@
-class Solution(object):
-    def largestIsland(self, grid):
-        """
-        :type grid: List[List[int]]
-        :rtype: int
-        """
+class Solution:
+    def largestIsland(self, grid: List[List[int]]) -> int:
         class DSU:
-            def __init__(self, n):
-                self.parent = [i for i in range(n)]
-                self.size = [1 for i in range(n)]
+            def __init__(self, V):
+                self.V = V
+                self.parent = [i for i in range(V)]
+                # print(self.parent)
+                self.size = [1 for i in range(V)]
             
-            def get_parent(self, node):
+            def find_parent(self, node):
                 if node == self.parent[node]:
                     return node
-                self.parent[node] = self.get_parent(self.parent[node])
+                self.parent[node] = self.find_parent(self.parent[node])
                 return self.parent[node]
             
+            def union(self, u, v):
+                u = self.find_parent(u)
+                v = self.find_parent(v)
 
-            
-            def union(self, x, y):
-                uX = self.get_parent(x)
-                uY = self.get_parent(y)
-
-                if uX == uY:
+                if u == v:
                     return
-                
-                if self.size[uX] > self.size[uY]:
-                    self.parent[uY] = uX
-                    self.size[uX] += self.size[uY]
+
+                if self.size[u] > self.size[v]:
+                    self.size[u] += self.size[v]
+                    self.parent[v] = u
+                elif self.size[u] < self.size[v]:
+                    self.size[v] += self.size[u]
+                    self.parent[u] = v
                 else:
-                    self.parent[uX] = uY
-                    self.size[uY] += self.size[uX]
-                
-        
+                    self.size[v] += self.size[u]
+                    self.parent[u] = v
         ROWS = len(grid)
         COLS = len(grid[0])
-        dsu = DSU((ROWS * COLS) + 1)
-        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+        dsu = DSU(ROWS * COLS)
 
         for i in range(ROWS):
             for j in range(COLS):
                 if grid[i][j] == 1:
-                    for dx, dy in directions:
-                        nx, ny = i + dx, j + dy
+                    for dx, dy in [(-1, 0), (1, 0), (0, 1), (0, -1)]:
+                        nx = i + dx
+                        ny = j + dy
+
                         if nx >= 0 and nx < ROWS and ny >= 0 and ny < COLS:
                             if grid[nx][ny] == 1:
-                                coord1 = nx * ROWS + 1 + ny
-                                coord2 = i * ROWS + 1 + j
+                                coord1 = nx * COLS + ny
+                                coord2 = i * COLS + j
                                 dsu.union(coord1, coord2)
-        maxSize = 0
+        # print(dsu.parent)
+        maxi = float('-inf')
         for i in range(ROWS):
             for j in range(COLS):
                 if grid[i][j] == 0:
-                    components = set()
-                    for dx, dy in directions:
-                        nx, ny = i + dx, j + dy
+                    vset = set()
+                    for dx, dy in [(-1, 0), (1, 0), (0, 1), (0, -1)]:
+                        nx = i + dx
+                        ny = j + dy
+
                         if nx >= 0 and nx < ROWS and ny >= 0 and ny < COLS:
                             if grid[nx][ny] == 1:
-                                coord1 = nx * ROWS + 1 + ny
-                                components.add(dsu.get_parent(coord1))
-                    size = 1
-                    # print(i, j)
-                    # print(components)
-                    for comp in components:
-                        size += dsu.size[comp]
-                    maxSize = max(maxSize, size)
-        for i in range((ROWS * COLS) + 1):
-            maxSize = max(maxSize, dsu.size[i])
-        return maxSize
+                                vset.add(dsu.find_parent(nx * COLS + ny))
+                    ans = 0
+                    # print(vset)
+                    # print(dsu.size)
+                    for parent in vset:
+                        ans += dsu.size[parent]
+                    maxi = max(maxi, ans + 1)
+        if maxi == float('-inf'):
+            return ROWS * COLS
+        return maxi
