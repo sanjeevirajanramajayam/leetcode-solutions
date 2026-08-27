df1c80f37ad80afd71c69d3058bcf0347feb2459
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-08-27 09:12:25 +0530

    Time: 11 ms (66.63%) | Memory: 19.8 MB (10.29%) - LeetSync

--- 51-n-queens/n-queens.py
+++ 51-n-queens/n-queens.py
@@ -1,47 +1,42 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        board = [["." for x in range(n)] for j in range(n)]
-        ans = []
-        def isSafe(row, col):
-            # print(row, col)
-            # upper diagonal
-            trow = row
-            tcol = col
-            while trow >= 0 and tcol >= 0:
-                # print(trow, tcol, board[trow])
-                if board[trow][tcol] == 'Q':
-                    return False
-                trow -= 1
-                tcol -= 1
-            
-            for i in range(n):
-                if board[row][i] == 'Q':
-                    return False
-            
-            trow = row
-            tcol = col
-            while trow < n and tcol >= 0:
-                if board[trow][tcol] == 'Q':
-                    return False
-                trow += 1
-                tcol -= 1
-            return True
+        board =[['.' for i in range(n)] for i in range(n)]
+        rowSet = set()
+        upperDiag = set()
+        lowerDiag = set()
 
-                
-        def fn(col):
-            print(col)
+        def validBoard(row, col):
+            if row in rowSet:
+                return False
+
+            if row + col in upperDiag:
+                return False
+            
+            if row - col in lowerDiag:
+                return False
+            
+            return True
+        ans = []
+        def backtrack(col):
+            nonlocal ans
             if col == n:
-                ans.append([x[:] for x in board[:]])
-                return 
+                newBoard = []
+                for row in board:
+                    newBoard.append("".join(row))
+                ans.append(newBoard)
+                return
             for row in range(n):
-                if isSafe(row, col):
+                if validBoard(row, col):
                     board[row][col] = 'Q'
-                    fn(col + 1)
+                    rowSet.add(row)
+                    upperDiag.add(row + col)
+                    lowerDiag.add(row - col)
+
+                    backtrack(col + 1)
+
                     board[row][col] = '.'
-        fn(0)
-        for x in ans:
-            for j in range(len(x)):
-                x[j] = "".join(x[j])
-            #     print(x[j])
-            # print()
+                    rowSet.remove(row)
+                    upperDiag.remove(row + col)
+                    lowerDiag.remove(row - col)
+        backtrack(0)
         return ans
