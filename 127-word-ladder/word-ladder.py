0486b28c1ac87a3cc190e3cb1f3a2045c04a53ec
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-08-27 12:06:21 +0530

    Time: 199 ms (64.42%) | Memory: 20.4 MB (86.48%) - LeetSync

--- 127-word-ladder/word-ladder.py
+++ 127-word-ladder/word-ladder.py
@@ -1,19 +1,21 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        wordSet = set(wordList)
-        queue = deque([(1, beginWord)])
+        wordList = set(wordList)
+
+        queue = deque([(beginWord, 1)])
 
         while queue:
-            cnt, word = queue.popleft()
+            word, count = queue.popleft()
             if word == endWord:
-                return cnt
+                return count
             word = list(word)
             for i in range(len(word)):
-                orig = word[i]
+                origChar = word[i]
                 for ch in string.ascii_lowercase:
                     word[i] = ch
-                    if "".join(word) in wordSet:
-                        queue.append((cnt + 1, "".join(word)))
-                        wordSet.remove("".join(word))
-                word[i] = orig
+                    if "".join(word) in wordList:
+                        wordList.remove("".join(word))
+                        queue.append(("".join(word), count + 1))
+                word[i] = origChar
         return 0
+                     
