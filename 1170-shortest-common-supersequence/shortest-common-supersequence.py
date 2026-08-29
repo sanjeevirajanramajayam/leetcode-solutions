cba202dcb59ffb65241e8b6014ec98c288eb2248
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-08-29 14:47:56 +0530

    Time: 464 ms (19.09%) | Memory: 45.8 MB (20.79%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.py
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.py
@@ -1,37 +1,39 @@
 class Solution:
-    def shortestCommonSupersequence(self, s1: str, s2: str) -> str:
-        dp = [[0 for i in range(len(s2) + 1)] for i in range(len(s1) + 1)]
+    def shortestCommonSupersequence(self, str1: str, str2: str) -> str:
+        dp = [[0 for i in range(len(str2) + 1)] for i in range(len(str1) + 1)]
         
-        for i in range(1, len(s1) + 1):
-            for j in range(1, len(s2) + 1):
-                if s1[i - 1] == s2[j - 1]:
+        for i in range(1, len(str1) + 1):
+            for j in range(1, len(str2) + 1):
+                if str1[i - 1] == str2[j - 1]:
                     dp[i][j] = 1 + dp[i - 1][j - 1]
                 else:
                     dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
-        
-        i = len(s1)
-        j = len(s2)
-        ans = ""
+
         for x in dp:
             print(x)
+        
+        i = len(str1)
+        j = len(str2)
+        ans = ""
+
         while i > 0 and j > 0:
-            if s1[i - 1] == s2[j - 1]:
-                ans += s1[i - 1]
+            if str1[i - 1] == str2[j - 1]:
+                ans += str1[i - 1]
                 i -= 1
                 j -= 1
-            elif dp[i - 1][j] > dp[i][j - 1]:
-                ans += s1[i - 1]
-                i -= 1
+            elif dp[i][j - 1] > dp[i - 1][j]:
+                ans += str2[j - 1]
+                j -= 1
             else:
-                ans += s2[j - 1]
-                j -= 1
-        # print(i, j)
+                ans += str1[i - 1]
+                i -= 1
+        
         while i > 0:
-            ans += s1[i - 1]
+            ans += str1[i - 1]
             i -= 1
          
         while j > 0:
-            ans += s2[j - 1]
+            ans += str2[j - 1]
             j -= 1
         
         return ans[::-1]
