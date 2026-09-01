d19a1b7930c22eab7ccf65f300905cd66ca0880f
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-09-01 09:51:00 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (33.52%) - LeetSync

--- 1033-broken-calculator/broken-calculator.py
+++ 1033-broken-calculator/broken-calculator.py
@@ -1,10 +1,11 @@
 class Solution:
     def brokenCalc(self, startValue: int, target: int) -> int:
-        ans = 0
+        cnt = 0
         while target > startValue:
             if target % 2 == 0:
                 target //= 2
             else:
                 target += 1
-            ans += 1
-        return ans + abs(target - startValue) 
+            cnt += 1
+            # print(target, cnt)
+        return cnt + abs(startValue - target)
