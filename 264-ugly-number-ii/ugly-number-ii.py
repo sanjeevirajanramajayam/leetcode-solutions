36dd64a4b8054e321d37c46e5c39e498123ea4d9
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-09-01 13:52:13 +0530

    Time: 115 ms (9.71%) | Memory: 19.5 MB (29.58%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.py
+++ 264-ugly-number-ii/ugly-number-ii.py
@@ -1,12 +1,19 @@
 class Solution:
     def nthUglyNumber(self, n: int) -> int:
-        ugly = 1
+        heap = [1]
         visited = set()
-        heap = [ugly]
-        for _ in range(n):
-            num = heapq.heappop(heap)
+        if n == 1:
+            return 1
+        n -= 2
+        while n >= 0:
+            # print(heap)
+            val = heapq.heappop(heap)
+                # continue
             for factor in [2, 3, 5]:
-                if num * factor not in visited:
-                    visited.add(num * factor)
-                    heapq.heappush(heap, num * factor)
-        return num
+                if val * factor not in visited:
+                    heapq.heappush(heap, val * factor)
+
+                visited.add(val * factor)
+            # print(heap)
+            n -= 1
+        return heap[0]
