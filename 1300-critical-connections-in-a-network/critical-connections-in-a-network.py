a89d185fe45bfb60ae7c76b3d26eb1bec52ed187
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-09-01 16:11:32 +0530

    Time: 219 ms (61.08%) | Memory: 107 MB (19.50%) - LeetSync

--- 1300-critical-connections-in-a-network/critical-connections-in-a-network.py
+++ 1300-critical-connections-in-a-network/critical-connections-in-a-network.py
@@ -1,32 +1,30 @@
 class Solution:
     def criticalConnections(self, n: int, connections: List[List[int]]) -> List[List[int]]:
         timer = 0
-        time = [0] * n
-        low = [0] * n
-        visited = set()
-        timer = 0
+        time = [0 for i in range(n)]
+        low = [0 for i in range(n)]
+
         adjList = [[] for i in range(n)]
-        for startNode, endNode in connections:
-            adjList[startNode].append(endNode)
-            adjList[endNode].append(startNode)
+
+        for a, b in connections:
+            adjList[a].append(b)
+            adjList[b].append(a)
         ans = []
+        visited = set()
         def dfs(node, parent):
             nonlocal ans, timer
-            # print(node, parent)
             time[node] = low[node] = timer
             timer += 1
             visited.add(node)
             for nnode in adjList[node]:
-                # print(nnode)
                 if nnode == parent:
                     continue
                 if nnode not in visited:
                     dfs(nnode, node)
-                    low[node] = min(low[nnode], low[node])
                     if low[nnode] > time[node]:
                         ans.append([node, nnode])
+                    low[node] = min(low[node], low[nnode])
                 else:
-                    low[node] = min(low[nnode], low[node])
-
+                    low[node] = min(low[node], low[nnode])
         dfs(0, -1)
         return ans
