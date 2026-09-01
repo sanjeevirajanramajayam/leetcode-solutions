3b7ccf42b8b960423cf1f5c8356f68a8b53a9341
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-09-01 09:29:34 +0530

    Time: 319 ms (55.26%) | Memory: 147.6 MB (5.01%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.py
+++ 44-wildcard-matching/wildcard-matching.py
@@ -2,21 +2,23 @@ class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         @cache
         def fn(i, j):
-            # print(i, j)
-            if j < 0:
-                return i < 0
-
             if i < 0:
-                for k in range(j + 1):
+                for k in range(0, j + 1):
                     if p[k] != '*':
                         return False
                 return True
+
+            if j < 0:
+                return i < 0
+            
 
             if s[i] == p[j]:
                 return fn(i - 1, j - 1)
             elif p[j] == '?':
                 return fn(i - 1, j - 1)
-            elif p[j] == "*":
+            elif p[j] == '*':
                 return fn(i, j - 1) or fn(i - 1, j)
-            return False
+            else:
+                return False
+        
         return fn(len(s) - 1, len(p) - 1)
