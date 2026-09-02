a3f96f266a66d47ea0a64ea7145bf04148eea912
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-09-02 09:33:28 +0530

    Time: 7 ms (12.93%) | Memory: 19.6 MB (42.81%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -3,39 +3,29 @@ class Solution:
         A, B = nums1, nums2
         if len(A) > len(B):
             A, B = B, A
+        
         low = 0
         high = len(A) - 1
-        totalLen = len(A) + len(B)
-        half = totalLen // 2
+        total = len(A) + len(B)
+        half = total
+        half //= 2
+
         while True:
             mid = (low + high) // 2
-            j = half - mid - 2
-            if mid + 1 >= len(A):
-                Aright = float('inf')
-            else:
-                Aright = A[mid + 1]
+            print(mid)
+            j = half - (mid + 1) - 1
+            Aleft = A[mid] if mid >= 0 else float('-inf')
+            Aright = A[mid + 1] if mid + 1 < len(A) else float('inf')
+            Bleft = B[j] if j>= 0 else float('-inf')
+            Bright = B[j + 1] if j + 1 < len(B) else float('inf')  
 
-            if mid < 0:
-                Aleft = float('-inf')
-            else:
-                Aleft = A[mid]
-
-            if j + 1 >= len(B):
-                Bright = float('inf')
-            else:
-                Bright = B[j + 1]
-
-            if j < 0:
-                Bleft = float('-inf')
-            else:
-                Bleft = B[j]
-            # print(Blef/t, Aright, Aleft, Bright)
-            if Bleft <= Aright and Aleft <= Bright:
-                if totalLen % 2 == 0:
+            if (Aleft <= Bright and Bleft <= Aright):
+                if total % 2 == 0:
                     return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
                 else:
                     return min(Aright, Bright)
-            elif Aleft > Bright:
+            elif (Aleft > Bright):
                 high = mid - 1
-            else:
+            else:  
                 low = mid + 1
+        return -1
