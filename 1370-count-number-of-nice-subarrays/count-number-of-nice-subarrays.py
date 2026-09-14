040ebce9df401094337450127bb17007481691ac
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-09-14 16:21:56 +0530

    Time: 154 ms (20.59%) | Memory: 24.8 MB (67.27%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.py
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.py
@@ -1,20 +1,18 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
         def lessThanK(k):
+            l = 0
             if k < 0:
                 return 0
             oddCnt = 0
-            l = 0
             ans = 0
             for r in range(len(nums)):
                 if nums[r] % 2 == 1:
                     oddCnt += 1
-
                 while oddCnt > k:
                     if nums[l] % 2 == 1:
                         oddCnt -= 1
                     l += 1
-                
                 ans += (r - l + 1)
             return ans
         return lessThanK(k) - lessThanK(k - 1)
