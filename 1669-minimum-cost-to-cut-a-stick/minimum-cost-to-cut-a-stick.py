30e1b9609e0fe5cf95c6a7a253468367e68f6c0d
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-09-14 18:39:32 +0530

    Time: 653 ms (45.41%) | Memory: 34.6 MB (11.80%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.py
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.py
@@ -1,17 +1,13 @@
 class Solution:
     def minCost(self, n: int, cuts: List[int]) -> int:
         cuts = [0] + cuts + [n]
-        # print(cuts)
         cuts.sort()
         @cache
         def fn(i, j):
             if i > j:
                 return 0
-            
-            mini = float('inf')
+            minCost = float('inf')
             for k in range(i, j + 1):
-                # print(i ,j)
-                cost = (cuts[j + 1] - cuts[i - 1]) + fn(i, k - 1) + fn(k + 1, j)
-                mini = min(cost, mini)
-            return mini
+                minCost = min(minCost, cuts[j + 1] - cuts[i - 1] + fn(i, k - 1) + fn(k + 1, j))
+            return minCost
         return fn(1, len(cuts) - 2)
