4fd3c85f6686bba69874a72e6999429bbca5aa58
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-09-14 18:19:13 +0530

    Time: 73 ms (90.02%) | Memory: 19.8 MB (16.15%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,19 +1,18 @@
 class Solution:
-    def characterReplacement(self, nums: str, k: int) -> int:
+    def characterReplacement(self, s: str, k: int) -> int:
+        l = 0
+        hash = {}
         maxFreq = float('-inf')
-        maxChar = -1
-        freqHash = {}
         ans = float('-inf')
         l = 0
-        for r in range(len(nums)):
-            freqHash[nums[r]] = freqHash.get(nums[r], 0) + 1
-            if freqHash[nums[r]] > maxFreq:
-                maxFreq = freqHash[nums[r]]
-                # maxChar = nums[r]
-            # print(freqHash)
-            # print(maxFreq)
+        for r in range(len(s)):
+            hash[s[r]] = hash.get(s[r], 0) + 1
+            maxFreq = max(maxFreq, hash[s[r]])
+            
             while maxFreq + k < (r - l + 1):
-                freqHash[nums[l]] -= 1
+                # print(l)
+                hash[s[l]] = hash.get(s[l], 0) - 1
                 l += 1
+            
             ans = max(ans, r - l + 1)
         return ans
