fe43a7e6320543d3b80d7e749aa7249fa669020b
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-09-14 19:35:15 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (7.31%) - LeetSync

--- 832-binary-tree-pruning/binary-tree-pruning.py
+++ 832-binary-tree-pruning/binary-tree-pruning.py
@@ -6,32 +6,15 @@
 #         self.right = right
 class Solution:
     def pruneTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        def isOne(root):
-            if not root:
-                return None
-            if root.val == 1:
-                return True
-            if isOne(root.left) is True:
-                return True
-            if isOne(root.right) is True:
-                return True
-            return None
         def fn(root):
             if not root:
+                return
+            
+            root.left = fn(root.left)
+            root.right = fn(root.right)
+
+            if root.val == 0 and root.left == None and root.right == None:
                 return None
-            # print(root, isOne(root))
-            # return None
-            if isOne(root) is None:
-                return None
-            # print(root, isOne(root))
-            if isOne(root.left) is True:
-                fn(root.left)
-            else:
-                root.left = None
-            if isOne(root.right) is True:
-                fn(root.right)
-            else:
-                root.right = None
-            # root.right = isOne(root.right)
+            
             return root
         return fn(root)
