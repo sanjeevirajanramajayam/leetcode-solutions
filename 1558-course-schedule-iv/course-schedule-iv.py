d3122cc71017b08b2198f9050bda7e572d5a3b11
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-09-15 10:24:30 +0530

    Time: 7 ms (53.23%) | Memory: 19.6 MB (71.57%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.py
+++ 1558-course-schedule-iv/course-schedule-iv.py
@@ -1,29 +1,29 @@
 class Solution:
     def checkIfPrerequisite(self, n: int, p: List[List[int]], q: List[List[int]]) -> List[bool]:
+        queue = deque([])
+        prereq = [set() for i in range(n)]
         adjList = [[] for i in range(n)]
         inorder = [0 for i in range(n)]
-        for s, e in p:
-            adjList[s].append(e)
-            inorder[e] += 1
-        ancestor = [set() for i in range(n)]
-        queue = deque([])
+        
+        for startNode, endNode in p:
+            adjList[startNode].append(endNode)
+            inorder[endNode ] += 1
+            prereq[endNode].add(startNode)
+        
         for i in range(n):
             if inorder[i] == 0:
-                queue.append((i, -1))
+                queue.append(i)
+        
         while queue:
-            node, anc = queue.popleft()
-                # print(ancestor[node])
-            for i in adjList[node]:
-                inorder[i] -= 1
-                ancestor[i].update(set([node]))
-                ancestor[i].update(set(ancestor[node]))
-                if inorder[i] == 0:
-                    queue.append((i, node))
-        # print(ancestor)
+            node = queue.popleft()
+            for nnode in adjList[node]:
+                for req in prereq[node]:
+                    prereq[nnode].add(req)
+                inorder[nnode] -= 1
+                if inorder[nnode] == 0:
+                    queue.append(nnode)
+        # print(prereq)
         ans = []
-        for s, e in q:
-            if s in ancestor[e]:
-                ans.append(True)
-            else:
-                ans.append(False)
+        for startNode, endNode in q:
+            ans.append(startNode in prereq[endNode])
         return ans
