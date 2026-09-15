e3aab7ab8eb3fd684fe4993106e7157ca90a4817
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-09-15 11:42:19 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (67.71%) - LeetSync

--- 556-next-greater-element-iii/next-greater-element-iii.py
+++ 556-next-greater-element-iii/next-greater-element-iii.py
@@ -1,27 +1,22 @@
 class Solution:
     def nextGreaterElement(self, n: int) -> int:
-        oldN = n
         n = list(str(n))
-        # print(n)
         i = len(n) - 1
-        while i>0 and  n[i-1] >= n[i]:
+        while i > 0 and n[i] <= n[i - 1]:
             i -= 1
-        # print(i)
         if i == 0:
-            return -1 
-        # print(i)  
-        pivot = i - 1
-        pNum = n[pivot]
-        temp = len(n) - 1
-        while temp >= 0 and n[temp] <= pNum:
-            temp -= 1           
-        # print(temp, n[temp])
-        n[pivot], n[temp] = n[temp], n[pivot]
-
-        n = n[:i] + n[i:][::-1]
-        # print(n)
-        if int("".join(n)) == oldN:
             return -1
-        if int("".join(n)) > 2**31 - 1:
+        
+        idx = i - 1
+        # print(i, idx)
+        curr = len(n) - 1
+        while n[curr] <= n[idx]:
+            curr -= 1
+        # print(curr) 
+        n[idx], n[curr] = n[curr], n[idx]
+        # print(n)
+        n[idx + 1:] = reversed(n[idx + 1:])
+        # print(n)
+        if int("".join(n)) >= 2**31:
             return -1
         return int("".join(n))
